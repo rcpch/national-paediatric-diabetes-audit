ac4b277e1ddc4cf8bff076ee70dc0f17fdc46ee5
eatyourpeas <eatyourpeasapps@gmail.com>
2023-12-23 11:42:02 +0000

    add nhs number validation

--- project/npda/serializers/patient_serializer.py
+++ project/npda/serializers/patient_serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
+import nhs_number
 from ..models import Patient
+
 
 # Serializers define the API representation.
 class PatientSerializer(serializers.HyperlinkedModelSerializer):
+    def validate_nhs_number(self, value):
+        if nhs_number.is_valid(str(value)):
+            return value
+        else:
+            raise serializers.ValidationError("Invalid NHS Number")
+
     class Meta:
         model = Patient
         fields = "__all__"
--- project/npda/serializers/visit_serializer.py
+++ project/npda/serializers/visit_serializer.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from ..models import Visit
 
+
 # Serializers define the API representation.
 class VisitSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Visit
         fields = "__all__"
+        depth = 1
