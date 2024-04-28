6ce45aa0734e95b57098bf412a5d3d3cb29fe0e5
eatyourpeas <simon@eatyourpeas.co.uk>
2024-04-28 22:52:05 +0100

    nav styling, login and otp templates

--- project/npda/urls.py
+++ project/npda/urls.py
@@ -2,7 +2,12 @@ from django.urls import path, include
 from django.contrib.auth.views import PasswordResetConfirmView, LoginView, LogoutView
 from django.contrib.auth import urls as auth_urls
 from project.npda.views import npda_users
-from project.npda.views.visit import VisitCreateView, VisitDeleteView, VisitUpdateView, patient_visits
+from project.npda.views.visit import (
+    VisitCreateView,
+    VisitDeleteView,
+    VisitUpdateView,
+    patient_visits,
+)
 from project.npda.forms.npda_user_form import NPDAUpdatePasswordForm
 
 from .views import *
@@ -39,25 +44,17 @@ urlpatterns = [
         PatientDeleteView.as_view(),
         name="patient-delete",
     ),
-    path(
-        "npda_users",
-        view=npda_users,
-        name='npda_users'
-    ),
-    path(
-        "npda_users/add",
-        view=NPDAUserCreateView.as_view(),
-        name='npdauser-create'
-    ),
+    path("npda_users", view=npda_users, name="npda_users"),
+    path("npda_users/add", view=NPDAUserCreateView.as_view(), name="npdauser-create"),
     path(
         "npda_users/<int:pk>/update",
         view=NPDAUserUpdateView.as_view(),
-        name='npdauser-update'
+        name="npdauser-update",
     ),
     path(
         "npda_users/<int:pk>/delete",
         view=NPDAUserDeleteView.as_view(),
-        name='npdauser-delete'
+        name="npdauser-delete",
     ),
     path("captcha/", include("captcha.urls")),
     path("account/", include(auth_urls)),
@@ -74,10 +71,6 @@ urlpatterns = [
         ),
         name="password_reset_confirm",
     ),
-    path(
-        "account/login", view=LoginView.as_view(), name='login'
-    ),
-    path(
-        "account/login", view=LogoutView.as_view(), name='logout'
-    )
+    path("account/login", view=LoginView.as_view(), name="login"),
+    path("account/logout", view=LogoutView.as_view(), name="logout"),
 ]
