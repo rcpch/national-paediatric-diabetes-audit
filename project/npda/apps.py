7c9d9b9be11ab03048f975c3a926290e1359b00a
eatyourpeas <simon@eatyourpeas.co.uk>
2024-05-05 19:23:42 +0100

    add signals.py and signal to apps.py

--- project/npda/apps.py
+++ project/npda/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class NpdaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "project.npda"
+
+    def ready(self) -> None:
+        import project.npda.signals
+
+        return super().ready()
