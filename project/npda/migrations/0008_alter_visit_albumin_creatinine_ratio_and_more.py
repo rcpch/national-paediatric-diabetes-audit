67df9b303b63be02859fb87371f0a5a11c5d7a84
Danny Cowen <danny.cowen9821@gmail.com>
2024-05-20 15:23:41 +0100

    Fix migration conflict after rebase

--- project/npda/migrations/0008_alter_visit_albumin_creatinine_ratio_and_more.py
+++ project/npda/migrations/0008_alter_visit_albumin_creatinine_ratio_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("npda", "0003_alter_npdauser_options_alter_patient_options_and_more"),
+        ("npda", "0007_alter_patient_errors_alter_visit_errors"),
     ]
 
     operations = [
