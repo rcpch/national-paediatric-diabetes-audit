5337e2d2fe4ada503cdbd19a4fd9d1a46808ab97
eatyourpeas <simon@eatyourpeas.co.uk>
2024-06-06 14:28:21 +0100

    revert nhs-organisations url to api.rcpch.ac.uk

--- project/settings.py
+++ project/settings.py
@@ -49,10 +49,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 RCPCH_CENSUS_PLATFORM_URL = os.getenv("RCPCH_CENSUS_PLATFORM_URL")
 RCPCH_CENSUS_PLATFORM_TOKEN = os.getenv("RCPCH_CENSUS_PLATFORM_TOKEN")
 # TODO #83  - Fix the broken env in Azure and remove hardcoded URL
-RCPCH_NHS_ORGANISATIONS_API_URL = os.getenv(
-    "RCPCH_NHS_ORGANISATIONS_API_URL",
-    "https://rcpch-nhs-organisations.azurewebsites.net",
-)
+RCPCH_NHS_ORGANISATIONS_API_URL = os.getenv("RCPCH_NHS_ORGANISATIONS_API_URL")
 RCPCH_NHS_ORGANISATIONS_API_URL = "https://rcpch-nhs-organisations.azurewebsites.net"
 
 # This is the NHS Spine services - it does not require authentication
