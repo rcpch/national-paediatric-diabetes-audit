6863e3dc427fa59c558a6d43d8e11d8a00abe08f
anchit-chandran <anchit97123@gmail.com>
2024-06-06 20:00:48 +0100

    stash
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/tests/factories/NPDAUserFactory.py
+++ project/npda/tests/factories/NPDAUserFactory.py
@@ -9,6 +9,7 @@ import factory
 
 # rcpch imports
 from project.npda.models import NPDAUser
+from project.npda.general_functions import get_nhs_organisation
 
 
 class NPDAUserFactory(factory.django.DjangoModelFactory):
@@ -16,3 +17,29 @@ class NPDAUserFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = NPDAUser
+        skip_postgeneration_save = True
+
+    email = factory.Sequence(lambda n: f"npda_test_user_{n}@nhs.net")
+    first_name = "Mandel"
+    surname = "Brot"
+    is_active = True
+    is_superuser = False
+    email_confirmed = True
+    organisation_employer = factory.LazyFunction(
+        lambda: get_nhs_organisation(ods_code="RP401")
+    )
+
+    # Add Groups
+    @factory.post_generation
+    def groups(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        # hook into post gen hook to set pass
+        self.set_password("pw")
+
+        if extracted:
+            for group in extracted:
+                self.groups.add(group)
+
+            self.save()
---
+++ project/npda/tests/factories/test_user_actions/test_user_creation.py
@@ -0,0 +1,14 @@
+"""Tests for creating users."""
+# python imports
+import pytest
+import logging
+
+# 3rd party imports
+
+
+# E12 imports
+
+@pytest.mark.django_db
+def test_user_creation():
+    """Test user creation."""
+    pass
