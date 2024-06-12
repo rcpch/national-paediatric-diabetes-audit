7c899166f783372e4b27fcded1effc888c680edc
Danny Cowen <danny.cowen9821@gmail.com>
2024-06-12 09:48:38 +0100

    Adds comments to retrieve_cohort_for_date function

--- project/npda/general_functions/cohort_for_date.py
+++ project/npda/general_functions/cohort_for_date.py
@@ -5,12 +5,14 @@ def retrieve_cohort_for_date(date_instance: date) -> int:
     """
     Returns the cohort for a given date.
     """
+    # Audit cohort start date is on the 1st April every year
     audit_start_date = date(date.today().year, 4, 1)
     if date_instance < audit_start_date:
         # The patient was audited in the previous year
         days_remaining = (audit_start_date - date_instance).days
     else:
         # The patient was audited in the current year
+        # Audit cohort end date is on the 31st of March of every year
         audit_end_date = date(date.today().year + 1, 3, 31)
         days_remaining = (audit_start_date - date_instance).days
     total_days = (audit_end_date - audit_start_date).days
