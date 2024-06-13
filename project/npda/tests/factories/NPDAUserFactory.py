2b22d05623befc9d14bcd15296464dc0b849cd10
anchit-chandran <anchit97123@gmail.com>
2024-06-13 09:38:18 +0100

    tidy up npda user factory
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/tests/factories/NPDAUserFactory.py
+++ project/npda/tests/factories/NPDAUserFactory.py
@@ -1,18 +1,39 @@
-"""Factory fn to create new NPDAUser.
-"""
+"""Factory function to create new NPDAUser."""
 
-# standard imports
-from datetime import timedelta
-
-# third-party imports
+# Standard imports
 import factory
 
-# rcpch imports
+# Project imports
 from project.npda.models import NPDAUser
+from project.npda.general_functions import get_nhs_organisation
 
 
 class NPDAUserFactory(factory.django.DjangoModelFactory):
-    """Dependency factory for creating a minimum viable NPDAUser."""
+    """Factory for creating a minimum viable NPDAUser."""
 
     class Meta:
         model = NPDAUser
+        skip_postgeneration_save = True
+
+    email = factory.Sequence(lambda n: f"npda_test_user_{n}@nhs.net")
+    first_name = "Mandel"
+    surname = "Brot"
+    is_active = True
+    is_superuser = False
+    email_confirmed = True
+    organisation_employer = factory.LazyFunction(lambda: get_nhs_organisation(ods_code="RP401"))
+
+    @factory.post_generation
+    def groups(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        # Set a default password
+        self.set_password("pw")
+
+        # Add the extracted groups if provided
+        if extracted:
+            for group in extracted:
+                self.groups.add(group)
+
+        self.save()
