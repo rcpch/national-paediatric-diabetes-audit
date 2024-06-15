dea777dd83af0d7f8954909d15a4d98ae65f7b73
anchit-chandran <anchit97123@gmail.com>
2024-06-15 16:44:09 +0100

    implements seeding npdauser test db
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/tests/factories/seed_users.py
+++ project/npda/tests/factories/seed_users.py
@@ -4,6 +4,7 @@ Seeds E12 Users in test db once per session.
 
 # Standard imports
 import pytest
+import logging
 
 # 3rd Party imports
 from django.contrib.auth.models import Group
@@ -18,16 +19,21 @@ from project.npda.tests.UserDataClasses import (
 )
 from project.npda.models import (
     NPDAUser,
-    # Organisation,
 )
 from .NPDAUserFactory import NPDAUserFactory
 from project.constants.user import (
     RCPCH_AUDIT_TEAM,
 )
+from project.npda.general_functions import get_nhs_organisation
+
+# Logging
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
 def seed_users_fixture(django_db_setup, django_db_blocker):
+
+    # Define user data to seed
     users = [
         test_user_audit_centre_administrator_data,
         test_user_audit_centre_clinician_data,
@@ -37,41 +43,45 @@ def seed_users_fixture(django_db_setup, django_db_blocker):
     ]
 
     with django_db_blocker.unblock():
-        # Don't repeat seed
-        if not NPDAUser.objects.exists():
-            # TEST_USER_ORGANISATION = Organisation.objects.get(
-            #     ods_code="RP401",
-            #     trust__ods_code="RP4",
-            # )
 
-            is_active = True
-            is_staff = False
-            is_rcpch_audit_team_member = False
-            is_rcpch_staff = False
+        # Don't repeat seed if users already exist.
+        if NPDAUser.objects.exists():
+            logger.info("Test users already seeded. Skipping")
+            return
 
-            # seed a user of each type at GOSH
-            for user in users:
-                first_name = user.role_str
+        # Otherwise, seed the users
+        is_active = True
+        is_staff = False
+        is_rcpch_audit_team_member = False
+        is_rcpch_staff = False
 
-                # set RCPCH AUDIT TEAM MEMBER ATTRIBUTE
-                if user.role == RCPCH_AUDIT_TEAM:
-                    is_rcpch_audit_team_member = True
-                    is_rcpch_staff = True
+        # First get GOSH Organisation details
+        GOSH_ORGANISATION_DETAILS = get_nhs_organisation(ods_code="RP401")
+        GOSH = GOSH_ORGANISATION_DETAILS["name"]
 
-                if user.is_clinical_audit_team:
-                    is_rcpch_audit_team_member = True
-                    first_name = "CLINICAL_AUDIT_TEAM"
+        logger.info(f"Seeding test users at {GOSH}.")
+        # Seed a user of each type at GOSH
+        for user in users:
+            first_name = user.role_str
 
-                E12UserFactory(
-                    first_name=first_name,
-                    role=user.role,
-                    # Assign flags based on user role
-                    is_active=is_active,
-                    is_staff=is_staff,
-                    is_rcpch_audit_team_member=is_rcpch_audit_team_member,
-                    is_rcpch_staff=is_rcpch_staff,
-                    # organisation_employer=TEST_USER_ORGANISATION,
-                    groups=[user.group_name],
-                )
-        else:
-            print("Test users already seeded. Skipping")
+            # set RCPCH AUDIT TEAM MEMBER ATTRIBUTE
+            if user.role == RCPCH_AUDIT_TEAM:
+                is_rcpch_audit_team_member = True
+                is_rcpch_staff = True
+
+            if user.is_clinical_audit_team:
+                is_rcpch_audit_team_member = True
+                first_name = "CLINICAL_AUDIT_TEAM"
+
+            NPDAUserFactory(
+                first_name=first_name,
+                role=user.role,
+                is_active=is_active,
+                is_staff=is_staff,
+                is_rcpch_audit_team_member=is_rcpch_audit_team_member,
+                is_rcpch_staff=is_rcpch_staff,
+                organisation_employer=GOSH,
+                groups=[user.group_name],
+            )
+
+            logger.info(f"Seeded {first_name} at {GOSH}.")
