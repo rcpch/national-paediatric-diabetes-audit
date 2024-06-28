bd6ddd45d22d8ee4fe280fcf65b6c9d82bc00679
anchit-chandran <anchit97123@gmail.com>
2024-06-28 15:31:49 +0100

    revert logging settings env var
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/logging_settings.py
+++ project/logging_settings.py
@@ -4,9 +4,7 @@
 import os
 
 # LOGGING ENV VARS
-CONSOLE_LOG_LEVEL = (
-    "DEBUG"  # os.getenv("CONSOLE_LOG_LEVEL", "DEBUG")  # For npda specific logs
-)
+CONSOLE_LOG_LEVEL = os.getenv("CONSOLE_LOG_LEVEL", "DEBUG")  # For npda specific logs
 CONSOLE_DJANGO_LOG_LEVEL = os.getenv(
     "CONSOLE_DJANGO_LOG_LEVEL", "DEBUG"
 )  # For django logs
