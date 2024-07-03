70cddda79bc5a925432aa14ad40b9cc2edbccc9f
Danny Cowen <danny.cowen9821@gmail.com>
2024-07-03 12:16:08 +0200

    Creates CheckPDUMixin

--- project/npda/views/mixins.py
+++ project/npda/views/mixins.py
@@ -5,6 +5,9 @@ import logging
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import AccessMixin
+from django.http import HttpResponseForbidden
+
+from ..general_functions.retrieve_pdu import retrieve_pdu_from_organisation_ods_code
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +34,7 @@ class LoginAndOTPRequiredMixin(AccessMixin):
                 settings.DEBUG,
                 request.user.get_role_display(),
                 request.user.is_superuser,
+                # request.user.get_all_permissions(),
             )
             return super().dispatch(request, *args, **kwargs)
 
@@ -44,3 +48,31 @@ class LoginAndOTPRequiredMixin(AccessMixin):
             raise PermissionDenied()
 
         return super().dispatch(request, *args, **kwargs)
+
+
+class CheckPDUMixin:
+    def dispatch(self, request, *args, **kwargs):
+        # Check if the user is authenticated
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        
+        # get PDU assigned to user
+        user_pdu = retrieve_pdu_from_organisation_ods_code(request.user.organisation_employer)['pz_code']
+
+        # get pdu that user is requesting access of
+        requested_pdu = request.session.get("sibling_organisations").get("pz_code")
+
+        if request.user.is_superuser or (requested_pdu == user_pdu):
+            return super().dispatch(request, *args, **kwargs)
+        
+        else:
+            logger.info(
+                    "User %s is unverified. Tried accessing %s but only has access to %s",
+                    request.user,
+                    requested_pdu,
+                    user_pdu
+                )
+            raise PermissionDenied()
+    
+    def handle_no_permission(self):
+        return HttpResponseForbidden("You do not have permission to access this PDU")
