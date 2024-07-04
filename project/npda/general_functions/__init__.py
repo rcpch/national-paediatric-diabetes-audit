63e96ae3046205b2d57191126e867280bf7881e6
Danny Cowen <danny.cowen9821@gmail.com>
2024-07-04 17:00:36 +0200

    import pdu code into general_functions

--- project/npda/general_functions/__init__.py
+++ project/npda/general_functions/__init__.py
@@ -4,6 +4,7 @@ from .email import *
 from .group_for_group import *
 from .index_multiple_deprivation import *
 from .nhs_ods_requests import *
+from .pdus import *
 from .time_elapsed import *
 from .validate_dates import *
 from .validate_postcode import *
