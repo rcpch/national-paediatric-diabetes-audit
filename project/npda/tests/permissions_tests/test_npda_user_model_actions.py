e2a2b0d0b4b7cfadf042db9f6b24c1654487b716
anchit-chandran <anchit97123@gmail.com>
2024-07-10 13:25:36 +0100

    working test ensuring users can't view diff pdus
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ project/npda/tests/permissions_tests/test_npda_user_model_actions.py
@@ -0,0 +1,150 @@
+"""Tests for NPDAUser model actions."""
+
+# Python imports
+import pytest
+import logging
+from http import HTTPStatus
+
+# 3rd party imports
+from django.urls import reverse
+from django.test import Client
+
+# E12 imports
+from project.npda.models import NPDAUser
+from project.npda.tests.utils import set_session_attributes_for_signedin_user
+from project.npda.tests.UserDataClasses import test_user_rcpch_audit_team_data
+from project.npda.views.npda_users import NPDAUserListView
+
+
+logger = logging.getLogger(__name__)
+
+
+def login_test_user(client: Client, user, password: str = "pw"):
+    """
+    Helper function to log in a test user using the custom RCPCHLoginView.
+    """
+    login_url = reverse(
+        "two_factor:login"
+    )  # Adjust the URL name based on your configuration
+
+    # Simulate login POST request
+    response = client.post(
+        login_url,
+        {
+            "auth-username": user.email,
+            "auth-password": password,
+            "auth-remember": "on",
+        },
+    )
+
+    # Check if login was successful
+    assert response.status_code == 302  # Redirect after successful login
+
+    # The user should now be logged in and session data should be set
+    session = client.session
+    current_user_ods_code = user.organisation_employers.first().ods_code
+    current_user_pz_code = user.organisation_employers.first().pz_code
+
+    assert session["ods_code"] == current_user_ods_code
+    assert session["pz_code"] == current_user_pz_code
+    assert "sibling_organisations" in session
+    assert "organisation_choices" in session
+    assert "pdu_choices" in session
+
+    return client
+
+
+@pytest.mark.django_db
+def test_users_cannot_view_user_table_from_different_pdus(
+    seed_groups_fixture,
+    seed_users_fixture,
+    seed_patients_fixture,
+    client,
+):
+    """Except for RCPCH_AUDIT_TEAM, users cannot view the user table from different PDUs."""
+
+    ALDER_HEY_PZ_CODE = "PZ074"
+    ALDER_HEY_ODS_CODE = "RBS25"
+
+    # Get users which cannot view the user table from different PDUs.
+    test_users_ah = NPDAUser.objects.exclude(
+        first_name=test_user_rcpch_audit_team_data.role_str,
+    ).filter(organisation_employers__pz_code=ALDER_HEY_PZ_CODE)
+
+    test_user_count = test_users_ah.count()
+
+    # Check we have the correct number.
+    expected_test_user_count_excluding_rcpch_audit_team = 3
+    assert (
+        test_user_count == expected_test_user_count_excluding_rcpch_audit_team
+    ), f"Expected {expected_test_user_count_excluding_rcpch_audit_team} test users, got {test_user_count=}"
+
+    # For each user, ensure the queryset does not contain users from a PDU they are not part of Alder Hey (other seeded users are at GOSH).
+    for test_user in test_users_ah:
+
+        client = set_session_attributes_for_signedin_user(
+            client=client, user=test_user, ods_code=ALDER_HEY_ODS_CODE
+        )
+
+        # Simulate a GET request to the NPDAUserListView
+        url = reverse("npda_users")  
+        response = client.get(url)
+
+        # Check that the response is successful
+        assert response.status_code in [HTTPStatus.OK, HTTPStatus.FOUND]
+
+        # Extract the queryset from the context
+        view = NPDAUserListView()
+        view.request = response.wsgi_request
+        view.request.user = test_user  # Explicitly set the user in the request
+        queryset = view.get_queryset()
+
+        # Ensure the queryset does not contain users from a PDU they are not part of
+        assert (
+            queryset.exclude(organisation_employers__pz_code=ALDER_HEY_PZ_CODE).count()
+            == 0
+        ), f"User {test_user} from {ALDER_HEY_PZ_CODE=} can see NPDAUsers from other PDUs."
+
+
+@pytest.mark.django_db
+def test_rcpch_audit_team_can_view_all_npdausers(
+    seed_groups_fixture,
+    seed_users_fixture,
+    seed_patients_fixture,
+    client,
+):
+    """RCPCH_AUDIT_TEAM users can view all users."""
+
+    # Get an RCPCH_AUDIT_TEAM user
+    test_user_rcpch_audit_team = NPDAUser.objects.filter(
+        first_name=test_user_rcpch_audit_team_data.role_str
+    ).first()
+
+    # Get all users
+    all_users = NPDAUser.objects.all()
+    all_user_count = all_users.count()
+
+    # Set the session for the RCPCH_AUDIT_TEAM user
+    client = set_session_attributes_for_signedin_user(
+        client=client, user=test_user_rcpch_audit_team, ods_code="RP401"
+    )
+
+    # Simulate a GET request to the NPDAUserListView
+    url = reverse("npda_users")  
+    response = client.get(url)
+
+    # Check that the response is successful
+    assert response.status_code in [HTTPStatus.OK, HTTPStatus.FOUND]
+
+    # Extract the queryset from the context
+    view = NPDAUserListView()
+    view.request = response.wsgi_request
+    view.request.user = test_user_rcpch_audit_team  # Explicitly set the user in the request
+    queryset = view.get_queryset()
+    
+    print(f"{view.request.user.view_preference=}")
+
+    # Ensure the queryset contains all users
+    assert (
+        queryset.count() == all_user_count
+    ), f"RCPCH_AUDIT_TEAM user cannot see all users. Expected {all_user_count}, got {queryset.count()}."
--- project/npda/tests/utils.py
+++ project/npda/tests/utils.py
@@ -2,7 +2,6 @@
 from django_otp import DEVICE_ID_SESSION_KEY
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.test.client import RequestFactory
-from django.urls import reverse
 from two_factor.utils import default_device
 
 # NPDA Imports
@@ -21,28 +20,47 @@ def twofactor_signin(client, test_user) -> None:
     session.save()
 
 
-def set_session_attributes_for_user(client, user):
-    """Helper fn to set session attributes for user, as done during login."""
+def create_session_object_from_ods_code(ods_code: str) -> dict:
+    """Helper function to create a session object from an ODS code."""
+    sibling_organisations = get_single_pdu_from_ods_code(ods_code)
+
+    session = {
+        "ods_code": ods_code,
+        "sibling_organisations": sibling_organisations,
+        "organisation_choices": [
+            (choice["ods_code"], choice["name"])
+            for choice in sibling_organisations["organisations"]
+        ],
+        "pdu_choices": get_all_pdus_list_choices(),
+    }
+
+    return session
+
+
+def set_session_attributes_for_signedin_user(
+    client, user, ods_code: str = "RP401"
+):
+    """Helper function to set session attributes for a signed-in user, as done during login."""
+    # Log in the user
     client.login(username=user.email, password="pw")
 
-    request = RequestFactory().get(reverse("home"))
+    # Create a request to initiate the session
+    request = RequestFactory().get('/')
+
+    # Add the session middleware to process the request
     middleware = SessionMiddleware(lambda request: None)
     middleware.process_request(request)
     request.session.save()
 
-    sibling_organisations = get_single_pdu_from_ods_code("RP401")
-
-    request.session["ods_code"] = user.organisation_employer
-    request.session["sibling_organisations"] = sibling_organisations
-    request.session["organisation_choices"] = [
-        (choice["ods_code"], choice["name"])
-        for choice in sibling_organisations["organisations"]
-    ]
-    request.session["pdu_choices"] = get_all_pdus_list_choices()
+    # Update session data
+    session_data = create_session_object_from_ods_code(ods_code)
+    request.session.update(session_data)
     request.session.save()
 
+    # Update the client cookies to use the new session
     client.cookies["sessionid"] = request.session.session_key
 
-    # OTP
+    # OTP Log in (assumed to be a custom function)
     twofactor_signin(client, user)
+
     return client
