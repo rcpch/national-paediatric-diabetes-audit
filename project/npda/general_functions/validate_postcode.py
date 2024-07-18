3d438343bc131f5d8c3c01ce107d075cc60642d8
Michael Barton <michael@mbarton.cc>
2024-07-18 09:52:14 +0100

    make validate_postcode async

--- project/npda/general_functions/validate_postcode.py
+++ project/npda/general_functions/validate_postcode.py
@@ -3,8 +3,7 @@
 # django
 
 # third party libraries
-import requests
-from requests.exceptions import HTTPError
+import httpx
 
 # npda imports
 from django.conf import settings
@@ -12,7 +11,7 @@ from django.conf import settings
 from asgiref.sync import async_to_sync
 
 
-async def avalidate_postcode(postcode):
+async def avalidate_postcode(postcode, async_client):
     """
     Tests if postcode is valid
     Returns boolean
@@ -21,16 +20,20 @@ async def avalidate_postcode(postcode):
     request_url = f"{settings.POSTCODE_API_BASE_URL}/postcodes/{postcode}.json"
 
     try:
-        response = requests.get(
+        response = await async_client.get(
             url=request_url,
             timeout=10,  # times out after 10 seconds
         )
         response.raise_for_status()
-    except HTTPError as e:
+    except httpx.HTTPError as e:
         print(e.response.text)
         return False
 
     return True
 
-
-validate_postcode = async_to_sync(avalidate_postcode)
+def validate_postcode(postcode):
+    async def wrapper():
+        async with httpx.AsyncClient() as client:
+            return avalidate_postcode(postcode, client)
+    
+    return async_to_sync(wrapper)()
