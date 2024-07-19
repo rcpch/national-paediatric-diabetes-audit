155431efbcf29da7aaca0ff5bca1ae0bd3e7364f
anchit-chandran <anchit97123@gmail.com>
2024-07-19 18:15:06 +0100

    add audit_cohort to admin
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/admin.py
+++ project/npda/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from .models import NPDAUser, OrganisationEmployer, Patient, Site, Visit, VisitActivity
+from .models import (
+    NPDAUser,
+    OrganisationEmployer,
+    Patient,
+    Site,
+    Visit,
+    VisitActivity,
+    AuditCohort,
+)
 from django.contrib.sessions.models import Session
+
+
+@admin.register(AuditCohort)
+class AuditCohortAdmin(admin.ModelAdmin):
+    search_fields = ("audit_year", "quarter", "pz_code", "ods_code")
 
 
 @admin.register(OrganisationEmployer)
@@ -33,37 +46,46 @@ class VisitAdmin(admin.ModelAdmin):
 class VisitActivityAdmin(admin.ModelAdmin):
     search_fields = ("activity_datetime", "pk", "ip_address")
 
+
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
-    
-    list_display = ['session_key', 'user_id', 'ods_code', 'pz_code', 'organisation_choices', 'pdu_choices', 'expire_date']
+
+    list_display = [
+        "session_key",
+        "user_id",
+        "ods_code",
+        "pz_code",
+        "organisation_choices",
+        "pdu_choices",
+        "expire_date",
+    ]
 
     def session_data(self, obj):
         return obj.get_decoded()
 
-    session_data.short_description = 'Session Data'
-    
+    session_data.short_description = "Session Data"
+
     # Define the fields to be displayed in the admin panel
     def user_id(self, obj):
-        return self.session_data(obj).get('_auth_user_id', 'N/A')
+        return self.session_data(obj).get("_auth_user_id", "N/A")
 
     def ods_code(self, obj):
-        return self.session_data(obj).get('ods_code', 'N/A')
+        return self.session_data(obj).get("ods_code", "N/A")
 
     def pz_code(self, obj):
-        return self.session_data(obj).get('pz_code', 'N/A')
+        return self.session_data(obj).get("pz_code", "N/A")
 
     def organisation_choices(self, obj):
-        return self.session_data(obj).get('organisation_choices', 'N/A')
+        return self.session_data(obj).get("organisation_choices", "N/A")
 
     def pdu_choices(self, obj):
-        return self.session_data(obj).get('pdu_choices', 'N/A')
+        return self.session_data(obj).get("pdu_choices", "N/A")
 
-    user_id.short_description = 'User ID'
-    ods_code.short_description = 'ODS Code'
-    pz_code.short_description = 'PZ Code'
-    organisation_choices.short_description = 'Organisation Choices'
-    pdu_choices.short_description = 'PDU Choices'
+    user_id.short_description = "User ID"
+    ods_code.short_description = "ODS Code"
+    pz_code.short_description = "PZ Code"
+    organisation_choices.short_description = "Organisation Choices"
+    pdu_choices.short_description = "PDU Choices"
 
 
 admin.site.site_header = "RCPCH National Paediatric Diabetes Audit Admin"
