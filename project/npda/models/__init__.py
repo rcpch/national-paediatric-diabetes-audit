01f5d34f0fc079f7f1b33ffa71181bce2f05b78a
eatyourpeas <simon@eatyourpeas.co.uk>
2024-07-25 18:38:12 +0100

    refactor site to transfer in mixins

--- project/npda/models/__init__.py
+++ project/npda/models/__init__.py
@@ -2,7 +2,7 @@ from .help_text_mixin import *
 from .npda_user import *
 from .organisation_employer import *
 from .patient import *
-from .site import *
+from .transfer import *
 from .submission import *
 from .time_and_user_abstract_base_classes import *
 from .visit import *
