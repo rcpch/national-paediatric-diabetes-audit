46dbf17a0fad596232e29de60cf580ba30b1f070
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-01 22:08:15 +0100

    pseudocode submissionslistview tests

---
+++ project/npda/tests/model_tests/test_submissions.py
@@ -0,0 +1,7 @@
+"""
+This file contains tests for the submissions model.
+
+ -  SubmissionsListView GET request should return the active submissions for the PDU & ODS code in the session
+ -  SubmissionsListView GET request should not return the active submissions for a PDU & ODS code not in the session
+- SubmissionsListView POST request with param "submit-data" of value "delete-data" should delete the submission
+"""
