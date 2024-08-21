6e1cf5b32e10c30dac457bff01019e7abfb6a789
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-21 09:13:40 +0100

    add csv_summary to submissions template filtered to PDU toggle between national and PDU view for submissions break csv_summarize into own file and rename Add csv_summarize data to context in submission Rename view_preference to PDU View and National View

--- project/npda/general_functions/csv_upload.py
+++ project/npda/general_functions/csv_upload.py
@@ -1,13 +1,9 @@
 # python imports
 from datetime import date
-import time
 import logging
-import os
-from typing import Literal
 
 # django imports
 from django.apps import apps
-from django.conf import settings
 from django.utils import timezone
 from django.core.exceptions import ValidationError
 
@@ -20,15 +16,10 @@ from ...constants import (
     ALL_DATES,
 )
 
-from .validate_postcode import validate_postcode
-from .nhs_ods_requests import gp_details_for_ods_code
-from .quarter_for_date import retrieve_quarter_for_date
-
-from ..forms.patient_form import PatientForm
-from ..forms.visit_form import VisitForm
-
 # Logging setup
 logger = logging.getLogger(__name__)
+from ..forms.patient_form import PatientForm
+from ..forms.visit_form import VisitForm
 
 
 def csv_upload(user, csv_file=None, pdu_pz_code=None):
@@ -111,6 +102,7 @@ def csv_upload(user, csv_file=None, pdu_pz_code=None):
         ) | {"paediatric_diabetes_unit": pdu}
 
     def validate_patient_using_form(row):
+
         fields = row_to_dict(
             row,
             Patient,
@@ -132,6 +124,7 @@ def csv_upload(user, csv_file=None, pdu_pz_code=None):
         return form
 
     def validate_visit_using_form(patient, row):
+
         fields = row_to_dict(
             row,
             Visit,
@@ -282,38 +275,3 @@ def csv_upload(user, csv_file=None, pdu_pz_code=None):
 
     if errors_to_return:
         raise ValidationError(errors_to_return)
-
-
-def csv_summarise(csv_file):
-    """
-    This function takes a csv file and processes the file to create a summary of the data
-    It returns a dictionary with the status of the operation and the summary data
-    """
-    Patient = apps.get_model("npda", "Patient")
-
-    dataframe = pd.read_csv(
-        csv_file, parse_dates=ALL_DATES, dayfirst=True, date_format="%d/%m/%Y"
-    )
-
-    total_records = len(dataframe)
-    number_unique_nhs_numbers = dataframe["NHS Number"].nunique()
-    unique_nhs_numbers_no_spaces = (
-        dataframe["NHS Number"].fillna("").apply(lambda x: x.replace(" ", "")).unique()
-    )
-    count_of_records_per_nhs_number = dataframe["NHS Number"].value_counts()
-    matching_patients_in_current_audit_year = Patient.objects.filter(
-        nhs_number__in=list(unique_nhs_numbers_no_spaces),
-        submissions__submission_active=True,
-        submissions__audit_year=date.today().year,
-    ).count()
-
-    summary = {
-        "total_records": total_records,
-        "number_unique_nhs_numbers": number_unique_nhs_numbers,
-        "count_of_records_per_nhs_number": list(
-            count_of_records_per_nhs_number.items()
-        ),
-        "matching_patients_in_current_audit_year": matching_patients_in_current_audit_year,
-    }
-
-    return summary
