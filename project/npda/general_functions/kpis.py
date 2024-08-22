397b1755b951e6298bf439d25f1f7b80d9ad62e7
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-22 21:31:56 +0100

    update __init__.py, call each kpi function in kpis.py

--- project/npda/general_functions/kpis.py
+++ project/npda/general_functions/kpis.py
@@ -24,6 +24,63 @@ from project.npda.general_functions.kpis import kpi_7_total_new_diagnoses_t1dm
 from project.npda.general_functions.kpis import kpi_8_total_deaths
 from project.npda.general_functions.kpis import kpi_9_total_service_transitions
 from project.npda.general_functions.kpis import kpi_10_total_coeliacs
+from project.npda.general_functions.kpis import kpi_11_total_thyroids
+from project.npda.general_functions.kpis import kpi_12_total_ketone_test_equipment
+from project.npda.general_functions.kpis import kpi_13_one_to_three_injections_per_day
+from project.npda.general_functions.kpis import kpi_14_four_or_more_injections_per_day
+from project.npda.general_functions.kpis import kpi_15_insulin_pump
+from project.npda.general_functions.kpis import (
+    kpi_16_one_to_three_injections_plus_other_medication,
+)
+from project.npda.general_functions.kpis import (
+    kpi_17_four_or_more_injections_plus_other_medication,
+)
+from project.npda.general_functions.kpis import (
+    kpi_18_insulin_pump_plus_other_medication,
+)
+from project.npda.general_functions.kpis import kpi_19_dietary_management_alone
+from project.npda.general_functions.kpis import (
+    kpi_20_dietary_management_plus_other_medication,
+)
+from project.npda.general_functions.kpis import kpi_21_flash_glucose_monitor
+from project.npda.general_functions.kpis import kpi_22_real_time_cgm_with_alarms
+from project.npda.general_functions.kpis import kpi_23_type1_real_time_cgm_with_alarms
+from project.npda.general_functions.kpis import kpi_24_hybrid_closed_loop_system
+from project.npda.general_functions.kpis import kpi_25_hba1c
+from project.npda.general_functions.kpis import kpi_26_bmi
+from project.npda.general_functions.kpis import kpi_27_thyroid_screen
+from project.npda.general_functions.kpis import kpi_28_blood_pressure
+from project.npda.general_functions.kpis import kpi_29_urinary_albumin
+from project.npda.general_functions.kpis import kpi_30_retinal_screening
+from project.npda.general_functions.kpis import kpi_31_foot_examination
+from project.npda.general_functions.kpis import kpi_32_health_check_completion_rate
+from project.npda.general_functions.kpis import kpi_33_hba1c_4plus
+from project.npda.general_functions.kpis import kpi_34_psychological_assessment
+from project.npda.general_functions.kpis import kpi_35_smoking_status_screened
+from project.npda.general_functions.kpis import (
+    kpi_36_referral_to_smoking_cessation_service,
+)
+from project.npda.general_functions.kpis import (
+    kpi_37_additional_dietetic_appointment_offered,
+)
+from project.npda.general_functions.kpis import (
+    kpi_38_patients_attending_additional_dietetic_appointment,
+)
+from project.npda.general_functions.kpis import (
+    kpi_39_influenza_immunisation_recommended,
+)
+from project.npda.general_functions.kpis import kpi_40_sick_day_rules_advice
+from project.npda.general_functions.kpis import kpi_41_coeliac_disease_screening
+from project.npda.general_functions.kpis import kpi_42_thyroid_disease_screening
+from project.npda.general_functions.kpis import kpi_43_carbohydrate_counting_education
+from project.npda.general_functions.kpis import kpi_44_mean_hba1c
+from project.npda.general_functions.kpis import kpi_45_median_hba1c
+from project.npda.general_functions.kpis import kpi_46_number_of_admissions
+from project.npda.general_functions.kpis import kpi_47_number_of_dka_admissions
+from project.npda.general_functions.kpis import (
+    kpi_48_required_additional_psychological_support,
+)
+from project.npda.general_functions.kpis import kpi_49_albuminuria_present
 
 
 class CalculateKPIS:
@@ -264,6 +321,396 @@ class CalculateKPIS:
         Calculates KPI 10: Total number of coeliacs
         """
         return kpi_10_total_coeliacs(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_11(self) -> dict:
+        """
+        Calculates KPI 11: Total number of thyroid patients
+        """
+        return kpi_11_total_thyroids(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_12(self) -> dict:
+        """
+        Calculates KPI 12: Total number of patients with ketone test equipment
+        """
+        return kpi_12_total_ketone_test_equipment(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_13(self) -> dict:
+        """
+        Calculates KPI 13: Total number of patients on 1-3 injections per day
+        """
+        return kpi_13_one_to_three_injections_per_day(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_14(self) -> dict:
+        """
+        Calculates KPI 14: Total number of patients on 4 or more injections per day
+        """
+        return kpi_14_four_or_more_injections_per_day(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_15(self) -> dict:
+        """
+        Calculates KPI 15: Total number of patients on insulin pump
+        """
+        return kpi_15_insulin_pump(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_16(self) -> dict:
+        """
+        Calculates KPI 16: Total number of patients on 1-3 injections per day plus other medication
+        """
+        return kpi_16_one_to_three_injections_plus_other_medication(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_17(self) -> dict:
+        """
+        Calculates KPI 17: Total number of patients on 4 or more injections per day plus other medication
+        """
+        return kpi_17_four_or_more_injections_plus_other_medication(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_18(self) -> dict:
+        """
+        Calculates KPI 18: Total number of patients on insulin pump plus other medication
+        """
+        return kpi_18_insulin_pump_plus_other_medication(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_19(self) -> dict:
+        """
+        Calculates KPI 19: Total number of patients on dietary management alone
+        """
+        return kpi_19_dietary_management_alone(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_20(self) -> dict:
+        """
+        Calculates KPI 20: Total number of patients on dietary management plus other medication
+        """
+        return kpi_20_dietary_management_plus_other_medication(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_21(self) -> dict:
+        """
+        Calculates KPI 21: Total number of patients on flash glucose monitor
+        """
+        return kpi_21_flash_glucose_monitor(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_22(self) -> dict:
+        """
+        Calculates KPI 22: Total number of patients on real time CGM with alarms
+        """
+        return kpi_22_real_time_cgm_with_alarms(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_23(self) -> dict:
+        """
+        Calculates KPI 23: Total number of patients on type 1 real time CGM with alarms
+        """
+        return kpi_23_type1_real_time_cgm_with_alarms(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_24(self) -> dict:
+        """
+        Calculates KPI 24: Total number of patients on hybrid closed loop system
+        """
+        return kpi_24_hybrid_closed_loop_system(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_25(self) -> dict:
+        """
+        Calculates KPI 25: Total number of patients with HbA1c
+        """
+        return kpi_25_hba1c(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_26(self) -> dict:
+        """
+        Calculates KPI 26: Total number of patients with BMI
+        """
+        return kpi_26_bmi(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_27(self) -> dict:
+        """
+        Calculates KPI 27: Total number of patients with thyroid screen
+        """
+        return kpi_27_thyroid_screen(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_28(self) -> dict:
+        """
+        Calculates KPI 28: Total number of patients with blood pressure
+        """
+        return kpi_28_blood_pressure(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_29(self) -> dict:
+        """
+        Calculates KPI 29: Total number of patients with urinary albumin
+        """
+        return kpi_29_urinary_albumin(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_30(self) -> dict:
+        """
+        Calculates KPI 30: Total number of patients with retinal screening
+        """
+        return kpi_30_retinal_screening(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_31(self) -> dict:
+        """
+        Calculates KPI 31: Total number of patients with foot examination
+        """
+        return kpi_31_foot_examination(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_32(self) -> dict:
+        """
+        Calculates KPI 32: Total number of patients with health check completion rate
+        """
+        return kpi_32_health_check_completion_rate(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_33(self) -> dict:
+        """
+        Calculates KPI 33: Total number of patients with HbA1c 4+
+        """
+        return kpi_33_hba1c_4plus(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_34(self) -> dict:
+        """
+        Calculates KPI 34: Total number of patients with psychological assessment
+        """
+        return kpi_34_psychological_assessment(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_35(self) -> dict:
+        """
+        Calculates KPI 35: Total number of patients with smoking status screened
+        """
+        return kpi_35_smoking_status_screened(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_36(self) -> dict:
+        """
+        Calculates KPI 36: Total number of patients with referral to smoking cessation service
+        """
+        return kpi_36_referral_to_smoking_cessation_service(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_37(self) -> dict:
+        """
+        Calculates KPI 37: Total number of patients with additional dietetic appointment offered
+        """
+        return kpi_37_additional_dietetic_appointment_offered(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_38(self) -> dict:
+        """
+        Calculates KPI 38: Total number of patients attending additional dietetic appointment
+        """
+        return kpi_38_patients_attending_additional_dietetic_appointment(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_39(self) -> dict:
+        """
+        Calculates KPI 39: Total number of patients with influenza immunisation recommended
+        """
+        return kpi_39_influenza_immunisation_recommended(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_40(self) -> dict:
+        """
+        Calculates KPI 40: Total number of patients with sick day rules advice
+        """
+        return kpi_40_sick_day_rules_advice(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_41(self) -> dict:
+        """
+        Calculates KPI 41: Total number of patients with coeliac disease screening
+        """
+        return kpi_41_coeliac_disease_screening(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_42(self) -> dict:
+        """
+        Calculates KPI 42: Total number of patients with thyroid disease screening
+        """
+        return kpi_42_thyroid_disease_screening(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_43(self) -> dict:
+        """
+        Calculates KPI 43: Total number of patients with carbohydrate counting education
+        """
+        return kpi_43_carbohydrate_counting_education(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_44(self) -> dict:
+        """
+        Calculates KPI 44: Total number of patients with mean HbA1c
+        """
+        return kpi_44_mean_hba1c(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_45(self) -> dict:
+        """
+        Calculates KPI 45: Total number of patients with median HbA1c
+        """
+        return kpi_45_median_hba1c(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_46(self) -> dict:
+        """
+        Calculates KPI 46: Total number of patients with number of admissions
+        """
+        return kpi_46_number_of_admissions(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_47(self) -> dict:
+        """
+        Calculates KPI 47: Total number of patients with number of DKA admissions
+        """
+        return kpi_47_number_of_dka_admissions(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_48(self) -> dict:
+        """
+        Calculates KPI 48: Total number of patients with required additional psychological support
+        """
+        return kpi_48_required_additional_psychological_support(
+            patients=self.patients,
+            audit_start_date=self.audit_start_date,
+            audit_end_date=self.audit_end_date,
+        )
+
+    def calculate_kpi_49(self) -> dict:
+        """
+        Calculates KPI 49: Total number of patients with albuminuria present
+        """
+        return kpi_49_albuminuria_present(
             patients=self.patients,
             audit_start_date=self.audit_start_date,
             audit_end_date=self.audit_end_date,
