972e175b97e0e89a7bf6e678c8b5aad0b27b0118
anchit-chandran <anchit97123@gmail.com>
2024-08-23 16:27:43 +0100

    Reverts patient model back to previous state before overriding default django behaviours to implement validation error mixin.
    Keeps error enums
    
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/models/patient.py
+++ project/npda/models/patient.py
@@ -1,26 +1,21 @@
 # python imports
 from datetime import date
 import logging
+from enum import Enum
 
 # django imports
-from django.apps import apps
 from django.contrib.gis.db import models
 from django.contrib.gis.db.models import CharField, DateField, PositiveSmallIntegerField
-from django.contrib.postgres.fields import ArrayField
-from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
-from project.npda.general_functions import validate_postcode
 from project.npda.models.custom_validators import validate_nhs_number
-from project.npda.models.validation_errors_mixin import ValidationErrorsMixin
 
 # npda imports
 from ...constants import (
     ETHNICITIES,
     DIABETES_TYPES,
     SEX_TYPE,
-    UNKNOWN_POSTCODES_NO_SPACES,
     CAN_LOCK_CHILD_PATIENT_DATA_FROM_EDITING,
     CAN_UNLOCK_CHILD_PATIENT_DATA_FROM_EDITING,
     CAN_OPT_OUT_CHILD_FROM_INCLUSION_IN_AUDIT,
@@ -33,10 +28,10 @@ from ..general_functions import (
 # Logging
 logger = logging.getLogger(__name__)
 
-from enum import Enum
 
 class PatientError(Enum):
     """NOT including nhs number as that error should prevent saving"""
+
     DOB_IN_FUTURE = "Date of birth cannot be in the future."
     PT_OLDER_THAN_19yo = "Patient is too old for the NPDA."
     INVALID_POSTCODE = "Postcode is invalid."
@@ -46,35 +41,18 @@ class PatientError(Enum):
     DIAGNOSIS_DATE_IN_FUTURE = "Diagnosis date cannot be in the future."
 
 
-class Patient(ValidationErrorsMixin, models.Model):
+class Patient(models.Model):
     """
     The Patient class.
 
     The index of multiple deprivation is calculated in the save() method using the postcode supplied and the
     RCPCH Census Platform
 
-    Custom methods age and age_days, returns the age.
-    
-    Uses the ValidationErrorsMixin to handle custom validation errors and update the 'errors' field.
+    Custom methods age and age_days, returns the age
     """
-    class Meta:
-        verbose_name = "Patient"
-        verbose_name_plural = "Patients"
-        ordering = (
-            "pk",
-            "nhs_number",
-        )
-        permissions = [
-            CAN_LOCK_CHILD_PATIENT_DATA_FROM_EDITING,
-            CAN_UNLOCK_CHILD_PATIENT_DATA_FROM_EDITING,
-            CAN_OPT_OUT_CHILD_FROM_INCLUSION_IN_AUDIT,
-        ]
-        
-    # the NHS number for England and Wales
-    nhs_number = CharField(  
-        "NHS Number", 
-        unique=True, 
-        validators=[validate_nhs_number], # Raise ValidationError & prevent saving
+
+    nhs_number = CharField(  # the NHS number for England and Wales
+        "NHS Number", unique=True, validators=[validate_nhs_number]
     )
 
     sex = models.IntegerField("Stated gender", choices=SEX_TYPE, blank=True, null=True)
@@ -99,13 +77,8 @@ class Patient(ValidationErrorsMixin, models.Model):
         null=True,
     )
 
-    # Separately define the diabetes type choices to allow for custom
-    # handling of invalid choices
-    DIABETES_TYPES = DIABETES_TYPES
     diabetes_type = PositiveSmallIntegerField(
-        verbose_name="Diabetes Type", 
-        blank=True,
-        null=True,
+        verbose_name="Diabetes Type", choices=DIABETES_TYPES
     )
 
     diagnosis_date = DateField(verbose_name="Date of Diabetes Diagnosis")
@@ -128,13 +101,22 @@ class Patient(ValidationErrorsMixin, models.Model):
         verbose_name="Record is valid", blank=False, null=False, default=False
     )
 
-    # Custom field to store validation errors
-    # This field is populated by the ValidationErrorsMixin
-    # JSON field of structure {field_name: [PatientError1 name, PatientError2 name, ...], ...}
     errors = models.JSONField(
         verbose_name="Validation errors", blank=True, null=True, default=None
     )
 
+    class Meta:
+        verbose_name = "Patient"
+        verbose_name_plural = "Patients"
+        ordering = (
+            "pk",
+            "nhs_number",
+        )
+        permissions = [
+            CAN_LOCK_CHILD_PATIENT_DATA_FROM_EDITING,
+            CAN_UNLOCK_CHILD_PATIENT_DATA_FROM_EDITING,
+            CAN_OPT_OUT_CHILD_FROM_INCLUSION_IN_AUDIT,
+        ]
 
     def __str__(self) -> str:
         return f"ID: {self.pk}, {self.nhs_number}"
@@ -168,73 +150,21 @@ class Patient(ValidationErrorsMixin, models.Model):
         if today_date is None:
             today_date = self.get_todays_date()
         return stringify_time_elapsed(self.date_of_birth, today_date)
-        
-    # Custom validation methods using ValidationErrorsMixin
-    def validate_fields(self):
-        """
-        Can add custom validation logic here using PatientError.
-        
-        """
-        
-        TODAY = self.get_todays_date()
-        
-        # == `date_of_birth` ==
-        if self.date_of_birth:
-            # dob can't be in the future
-            if self.date_of_birth > date.today():
-                self.add_error('date_of_birth', PatientError.DOB_IN_FUTURE)
-            
-            # patient is > than 19 years old
-            if self.age_days(TODAY) > 19 * 365:
-                self.add_error('date_of_birth', PatientError.PT_OLDER_THAN_19yo)
-                
-        
 
-        # == `postcode` ==
-        if self.postcode:
-            
-            # Invalid postcode
-            if not validate_postcode(self.postcode) or self.postcode.upper().replace(" ", "").replace("-", "") in UNKNOWN_POSTCODES_NO_SPACES:
-                self.add_error('postcode', PatientError.INVALID_POSTCODE)
-        
-        # == `date_of_diagnosis` ==
-        if self.diagnosis_date:
-            # diagnosis date can't be before dob
-            if self.diagnosis_date < self.date_of_birth:
-                self.add_error('diagnosis_date', PatientError.DIAGNOSIS_DATE_BEFORE_DOB)
-            
-            # diagnosis date can't be in the future
-            if self.diagnosis_date > TODAY:
-                self.add_error('diagnosis_date', PatientError.DIAGNOSIS_DATE_IN_FUTURE)
-        
-        # == `diabetes_type` ==
-        if self.diabetes_type not in self.DIABETES_TYPES:
-            self.add_error('diabetes_type', PatientError.INVALID_DIABETES_TYPE)
-            
-    
-    def save(self, *args, **kwargs):
-        # Custom logic to calculate deprivation score
+    def save(self, *args, **kwargs) -> None:
+        # calculate the index of multiple deprivation quintile if the postcode is present
+        # Skips the calculation if the postcode is on the 'unknown' list
         if self.postcode:
             try:
-                self.index_of_multiple_deprivation_quintile = imd_for_postcode(self.postcode)
+                self.index_of_multiple_deprivation_quintile = imd_for_postcode(
+                    self.postcode
+                )
             except Exception as error:
+                # Deprivation score not persisted if deprivation score server down
                 self.index_of_multiple_deprivation_quintile = None
-                self.add_error('index_of_multiple_deprivation_quintile', PatientError.DEPRIVATION_CALCULATION_FAILED)
+                print(
+                    f"Cannot calculate deprivation score for {self.postcode}: {error}"
+                )
 
-        # Call the save method in the mixin
+        self.full_clean()  # Trigger validation
         return super().save(*args, **kwargs)
-
-    # Custom handling for methods from ValidationErrorsMixin
-    def get_fields_with_custom_choice_handling(self):
-        return ['diabetes_type']
-    
-    # def handle_invalid_choice(self, field_name, error_message):
-    #     """
-    #     Custom handling for the choice fields.
-    #     """
-    #     if field_name == 'diabetes_type':
-    #         # Add the INVALID_DIABETES_TYPE error
-    #         self.add_error(field_name, PatientError.INVALID_DIABETES_TYPE)
-    #     else:
-    #         # For other fields, use the default behavior
-    #         super().handle_invalid_choice(field_name, error_message)
