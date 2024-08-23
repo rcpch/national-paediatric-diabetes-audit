f01e2814b883606338389c021ed8193a09d1df64
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-23 22:26:05 +0100

    fix coeliac kpi 10

--- project/npda/general_functions/kpis/kpi_10.py
+++ project/npda/general_functions/kpis/kpi_10.py
@@ -1,5 +1,6 @@
 from typing import List
 from django.apps import apps
+from django.apps import apps
 from django.db.models import Q
 
 
@@ -10,8 +11,11 @@ def kpi_10_total_coeliacs(
     Calculates KPI 10: Total number of coeliacs
     Number of eligible patients (measure 1) whose most recent observation for item 37 (based on visit date) is 1 = Yes
     """
-    eligible_patients = patients.objects.filter(
-        # TODO # This calculation is part of issue #88 discussion
+    Visit = apps.get_model("npda", "Visit")
+    eligible_visits = Visit.objects.filter(
+        Q(patient__in=patients)
+        & Q(gluten_free_diet=1)
+        & Q(visit_date__range=[audit_start_date, audit_end_date])
     ).distinct()
 
-    return eligible_patients.count()
+    return eligible_visits.count()
