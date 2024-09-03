4b2e9b71ed9d9c757c149deabd62362f78a70fe8
eatyourpeas <simon@eatyourpeas.co.uk>
2024-09-03 22:26:01 +0100

    Replace child class with function generating Patient object

--- project/npda/general_functions/data_generator.py
+++ project/npda/general_functions/data_generator.py
@@ -69,84 +69,34 @@ class HbA1cTargetRange(Enum):
     WELL_ABOVE = "Well Above Target"
 
 
-class Child:
+def create_fictional_patient(
+    sex=None,
+    ethnicity=None,
+    age_range=None,
+    diabetes_type=None,
+    diagnosis_date=None,
+    is_alive=None,
+):
+    """
+    Constructor for the Child class.
+    It creates a valid child object using these parameters as seed values to generate an instance with random values.
+    Optional parameters are randomised if not provided.
+    sex: int - 0, 1, 2, 9
+    age_range: AgeRange - Enum class representing the range of ages for children (0-4, 5-10, 11-15, 16-19, 20-25) y
+    diabetes_type: int - 1,2,3,4,5,99 ("Type 1 Insulin-Dependent Diabetes Mellitus", "Type 2 Non-Insulin Dependent Diabetes Mellitus", "Cystic Fibrosis Related Diabetes", "MODY (monogenic forms of diabetes)", "Other specified Diabetes Mellitus", "Unknown/unspecified")
+    diagnosis_date: date - date of diagnosis of the child: if not provided, a valid random date is generated. If provided, the date must be valid, and left here as a parameter so that the user can provide a specific date, for example in the latest quarter or audit year.
+    is_alive: bool - True or False
 
-    sex = random.choice(SEX_TYPE)[0]
-    diabetes_type = random.choice(DIABETES_TYPES)[0]
-    age_range = random.choice(list(AgeRange))
-    ethnicity = random.choice(ETHNICITIES)[0]
-    diabetes_type = random.choice(DIABETES_TYPES)[0]
-    postcode = "SW1A 1AA"  # random postcode
-    is_alive = True
+    Returns a Patient object - note it has not been saved as a record in the database. If invalid records are required, this instance can be modified to be invalid.
+    """
 
-    def __init__(
-        self,
-        sex=None,
-        ethnicity=None,
-        age_range=None,
-        diabetes_type=None,
-        diagnosis_date=None,
-        is_alive=None,
-    ):
-        """
-        Constructor for the Child class.
-        It creates a valid child object using these parameters as seed values to generate an instance with random values.
-        Optional parameters are randomised if not provided.
-        sex: int - 0, 1, 2, 9
-        age_range: AgeRange - Enum class representing the range of ages for children (0-4, 5-10, 11-15, 16-19, 20-25) y
-        diabetes_type: int - 1,2,3,4,5,99 ("Type 1 Insulin-Dependent Diabetes Mellitus", "Type 2 Non-Insulin Dependent Diabetes Mellitus", "Cystic Fibrosis Related Diabetes", "MODY (monogenic forms of diabetes)", "Other specified Diabetes Mellitus", "Unknown/unspecified")
-        diagnosis_date: date - date of diagnosis of the child: if not provided, a valid random date is generated. If provided, the date must be valid, and left here as a parameter so that the user can provide a specific date, for example in the latest quarter or audit year.
-        is_alive: bool - True or False
+    # Set the age range of the child to generate randomly if not provided
+    if age_range is None:
+        age_range = random.choice(list(AgeRange))
 
-        Returns a Patient object - note it has not been saved as a record in the database. If invalid records are required, this instance can be modified to be invalid.
-        """
+    # Private nested functions
 
-        # Set a random ethnicity if not provided
-        if ethnicity is not None:
-            self.ethnicity = ethnicity[0]
-
-        # Set the age range of the child to generate randomly if not provided
-        if age_range is not None:
-            self.age_range = age_range
-
-        # Use the age range to generate a date of birth
-        self.date_of_birth = self._random_date_of_birth(self.age_range)
-
-        # Use the date of birth to generate a date of diagnosis
-        if diagnosis_date is not None:
-            self.diagnosis_date = diagnosis_date
-        else:
-            self.diagnosis_date = self._random_date(self.date_of_birth, date.today())
-
-        # Set a random diabetes type if not provided
-        if diabetes_type is not None:
-            self.diabetes_type = diabetes_type[0]
-
-        # Generate a random date of death if the child is not alive
-        if is_alive is not None:
-            self.is_alive = is_alive
-            if not is_alive:
-                death_date = self._random_date(self.diagnosis_date, date.today())
-            else:
-                death_date = None
-        else:
-            death_date = None
-
-        # Create the child object
-        self.sex = sex if sex is not None else self.sex
-
-        self.death_date = death_date
-        self.nhs_number = nhs_number.generate(
-            quantity=1, for_region=nhs_number.REGION_ENGLAND
-        )[0]
-
-        self.index_of_multiple_deprivation_quintile = random.randint(1, 5)
-        self.gp_practice_ods_code = "G85004"  # random gp_practice_ods_code
-        self.gp_practice_postcode = "SE23 1HU"  # random gp_practice_postcode
-        self.is_valid = True
-        self.errors = []
-
-    def _random_date_of_birth(self, age_range):
+    def _random_date_of_birth(age_range):
         """
         Returns a random date of birth within the age range requested.
         """
@@ -173,40 +123,68 @@ class Child:
         random_days = random.randint(0, (end_date - start_date).days)
         return start_date + relativedelta(days=random_days)
 
-    def _random_date(self, start_date, end_date):
+    def _random_date(start_date, end_date):
         """
         Returns a random date between the start and end dates.
         """
         random_days = random.randint(0, (end_date - start_date).days)
         return start_date + relativedelta(days=random_days)
 
+    Patient = apps.get_model("npda", "Patient")
 
-def generate_valid_patients(
-    number_of_children: int,
-    sex=None,
-    ethnicity=None,
-    age_range=None,
-    diabetes_type=None,
-    diagnosis_date=None,
-    is_alive=None,
-):
-    """
-    Generates a list of fictional children as Patient objects.
-    Note that the children are not saved as records in the database and  therefore do not have a primary key.
-    """
-    children = []
-    for _ in range(number_of_children):
-        child = Child(
-            sex=sex,
-            ethnicity=ethnicity,
-            age_range=age_range,
-            diabetes_type=diabetes_type,
-            diagnosis_date=diagnosis_date,
-            is_alive=is_alive,
-        )
-        Patient = apps.get_model("npda", "Patient")
-        children.append(Patient(**child.__dict__))
-    return children
+    postcode = "SW1A 1AA"  # random postcode
+
+    # Set a random ethnicity if not provided
+    if ethnicity is None:
+        ethnicity = random.choice(ETHNICITIES)[0]
+
+    # Use the age range to generate a date of birth
+    date_of_birth = _random_date_of_birth(age_range=age_range)
+
+    # Use the date of birth to generate a date of diagnosis
+    if diagnosis_date is None:
+        diagnosis_date = _random_date(date_of_birth, date.today())
+
+    # Set a random diabetes type if not provided
+    if diabetes_type is None:
+        diabetes_type = random.choice(DIABETES_TYPES)[0]
+
+    # Generate a random date of death if the child is not alive
+    death_date = None
+    if is_alive is None:
+        is_alive = True
+    else:
+        if not is_alive:
+            death_date = _random_date(diagnosis_date, date.today())
+
+    # Create the child object
+    sex = sex if sex is None else random.choice(SEX_TYPE)[0]
+
+    _nhs_number = nhs_number.generate(quantity=1, for_region=nhs_number.REGION_ENGLAND)[
+        0
+    ]
+
+    index_of_multiple_deprivation_quintile = random.randint(1, 5)
+    gp_practice_ods_code = "G85004"  # random gp_practice_ods_code
+    gp_practice_postcode = "SE23 1HU"  # random gp_practice_postcode
+    is_valid = True
+    errors = []
+
+    return Patient(
+        nhs_number=_nhs_number,
+        postcode=postcode,
+        sex=sex,
+        date_of_birth=date_of_birth,
+        ethnicity=ethnicity,
+        index_of_multiple_deprivation_quintile=index_of_multiple_deprivation_quintile,
+        diabetes_type=diabetes_type,
+        diagnosis_date=diagnosis_date,
+        death_date=death_date,
+        gp_practice_ods_code=gp_practice_ods_code,
+        gp_practice_postcode=gp_practice_postcode,
+        is_valid=is_valid,
+        errors=errors,
+    )
 
 
 def generate_transfer_instance(patient, paediatric_diabetes_unit=None):
@@ -258,7 +236,7 @@ class Attendance:
 
     def __init__(
         self,
-        patient: Child,
+        patient,
         visit_date: date = None,
         hba1_target_range: HbA1cTargetRange = None,  # one of ["On Target", "Above Target", "Well Above Target"]
         visit_type: VisitType = VisitType.CLINIC,
