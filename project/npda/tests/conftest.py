d6785465f5958683a7e3f0ed6d469dbb2860db1d
anchit-chandran <anchit97123@gmail.com>
2024-09-21 11:43:27 +0100

    patch imd call, significantly boosts test speed
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/tests/conftest.py
+++ project/npda/tests/conftest.py
@@ -5,24 +5,22 @@ Configures pytest fixtures for npda app tests.
 # standard imports
 
 # third-party imports
-from pytest_factoryboy import register
-import pytest
+import logging
+from unittest.mock import patch
 
+import pytest
+from pytest_factoryboy import register
 
 # rcpch imports
-from project.npda.tests.factories import (
-    seed_groups_fixture,
-    seed_users_fixture,
-    seed_patients_fixture,
-    PatientFactory,
-    VisitFactory,
-    NPDAUserFactory,
-    OrganisationEmployerFactory,
-    PaediatricsDiabetesUnitFactory,
-    TransferFactory,
-)
+from project.npda.tests.factories import (NPDAUserFactory,
+                                          OrganisationEmployerFactory,
+                                          PaediatricsDiabetesUnitFactory,
+                                          PatientFactory, TransferFactory,
+                                          VisitFactory, seed_groups_fixture,
+                                          seed_patients_fixture,
+                                          seed_users_fixture)
 
-
+logger = logging.getLogger(__name__)
 # register factories to be used across test directory
 
 # factory object becomes lowercase-underscore form of the class name
@@ -32,3 +30,11 @@ register(NPDAUserFactory)  # => npdauser_factory
 register(OrganisationEmployerFactory)  # => npdauser_factory
 register(PaediatricsDiabetesUnitFactory)  # => npdauser_factory
 register(TransferFactory)  # => npdauser_factory
+
+@pytest.fixture(autouse=True)
+def patch_imd_for_postcode():
+    """Automatically patch `imd_for_postcode` for all tests."""
+    logger.debug("Patched imd_for_postcode returning 4")
+    with patch('project.npda.models.patient.imd_for_postcode', return_value=4) as mock_model:
+        print("Patching imd_for_postcode at models.patient:", mock_model)
+        yield
