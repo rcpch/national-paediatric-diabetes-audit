da005a8b2605a3abe35a8427602d44b410b5ec31
eatyourpeas <simon@eatyourpeas.co.uk>
2024-10-04 21:44:44 +0100

    remove print

--- project/npda/views/patient.py
+++ project/npda/views/patient.py
@@ -97,8 +97,6 @@ class PatientListView(
             else:
                 obj.latest_quarter = None
 
-        print(patient_queryset[0].errors)
-
         return patient_queryset
 
     def get_context_data(self, **kwargs):
