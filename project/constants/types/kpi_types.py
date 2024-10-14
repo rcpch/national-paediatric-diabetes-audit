4c3f150ad9b03438831127b56d20d6e31a6a2c2a
anchit-chandran <anchit97123@gmail.com>
2024-10-14 09:28:43 +0100

    adds kpi registry
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/constants/types/kpi_types.py
+++ project/constants/types/kpi_types.py
@@ -54,3 +54,279 @@ class KPICalculationsObject:
         str,
         KPIResult,
     ]
+
+
+"""
+Hard coding these for simplicty and readability
+
+KPIS
+1-12
+    - used as denominators for subsequent
+13-20
+    - treatment regimen
+21-23
+    - glucose monitoring
+24
+    - Hybrid closed loop system (HCL)
+25-32
+    - 7 key processes
+33-40
+    - additional processes
+41-43
+    - care at diagnosis
+44-49
+    - outcomes
+"""
+KPI_ATTRIBUTE_LABEL_MAP = {
+    1: {
+        "attribute_name": "kpi_1_total_eligible",
+        "rendered_label": "Total number of eligible patients",
+    },
+    2: {
+        "attribute_name": "kpi_2_total_new_diagnoses",
+        "rendered_label": "Total number of new diagnoses within the audit period",
+    },
+    3: {
+        "attribute_name": "kpi_3_total_t1dm",
+        "rendered_label": "Total number of eligible patients with Type 1 diabetes",
+    },
+    4: {
+        "attribute_name": "kpi_4_total_t1dm_gte_12yo",
+        "rendered_label": "Number of patients aged 12+ with Type 1 diabetes",
+    },
+    5: {
+        "attribute_name": "kpi_5_total_t1dm_complete_year",
+        "rendered_label": "Total number of patients with T1DM who have completed a year of care",
+    },
+    6: {
+        "attribute_name": "kpi_6_total_t1dm_complete_year_gte_12yo",
+        "rendered_label": "Total number of patients with T1DM who have completed a year of care and are aged 12 or older",
+    },
+    7: {
+        "attribute_name": "kpi_7_total_new_diagnoses_t1dm",
+        "rendered_label": "Total number of new diagnoses of T1DM",
+    },
+    8: {
+        "attribute_name": "kpi_8_total_deaths",
+        "rendered_label": "Number of patients who died within audit period",
+    },
+    9: {
+        "attribute_name": "kpi_9_total_service_transitions",
+        "rendered_label": "Number of patients who transitioned/left service within audit period",
+    },
+    10: {
+        "attribute_name": "kpi_10_total_coeliacs",
+        "rendered_label": "Total number of coeliacs",
+    },
+    11: {
+        "attribute_name": "kpi_11_total_thyroids",
+        "rendered_label": "Number of patients with thyroid disease",
+    },
+    12: {
+        "attribute_name": "kpi_12_total_ketone_test_equipment",
+        "rendered_label": "Number of patients with ketone test equipment",
+    },
+    13: {
+        "attribute_name": "kpi_13_one_to_three_injections_per_day",
+        "rendered_label": "Number of patients on one to three injections per day",
+    },
+    14: {
+        "attribute_name": "kpi_14_four_or_more_injections_per_day",
+        "rendered_label": "Number of patients on four or more injections per day",
+    },
+    15: {
+        "attribute_name": "kpi_15_insulin_pump",
+        "rendered_label": "Number of patients on insulin pump",
+    },
+    16: {
+        "attribute_name": "kpi_16_one_to_three_injections_plus_other_medication",
+        "rendered_label": "Number of patients on one to three injections plus other medication",
+    },
+    17: {
+        "attribute_name": "kpi_17_four_or_more_injections_plus_other_medication",
+        "rendered_label": "Number of patients on four or more injections plus other medication",
+    },
+    18: {
+        "attribute_name": "kpi_18_insulin_pump_plus_other_medication",
+        "rendered_label": "Number of patients on insulin pump plus other medication",
+    },
+    19: {
+        "attribute_name": "kpi_19_dietary_management_alone",
+        "rendered_label": "Number of patients on dietary management alone",
+    },
+    20: {
+        "attribute_name": "kpi_20_dietary_management_plus_other_medication",
+        "rendered_label": "Number of patients on dietary management plus other medication",
+    },
+    21: {
+        "attribute_name": "kpi_21_flash_glucose_monitor",
+        "rendered_label": "Number of patients on flash glucose monitor",
+    },
+    22: {
+        "attribute_name": "kpi_22_real_time_cgm_with_alarms",
+        "rendered_label": "Number of patients on real-time CGM with alarms",
+    },
+    23: {
+        "attribute_name": "kpi_23_type1_real_time_cgm_with_alarms",
+        "rendered_label": "Number of patients on Type 1 real-time CGM with alarms",
+    },
+    24: {
+        "attribute_name": "kpi_24_hybrid_closed_loop_system",
+        "rendered_label": "Number of patients on hybrid closed loop system",
+    },
+    25: {
+        "attribute_name": "kpi_25_hba1c",
+        "rendered_label": "Number of patients with HbA1c",
+    },
+    26: {
+        "attribute_name": "kpi_26_bmi",
+        "rendered_label": "Number of patients with BMI",
+    },
+    27: {
+        "attribute_name": "kpi_27_thyroid_screen",
+        "rendered_label": "Number of patients with thyroid screen",
+    },
+    28: {
+        "attribute_name": "kpi_28_blood_pressure",
+        "rendered_label": "Number of patients with blood pressure",
+    },
+    29: {
+        "attribute_name": "kpi_29_urinary_albumin",
+        "rendered_label": "Number of patients with urinary albumin",
+    },
+    30: {
+        "attribute_name": "kpi_30_retinal_screening",
+        "rendered_label": "Number of patients with retinal screening",
+    },
+    31: {
+        "attribute_name": "kpi_31_foot_examination",
+        "rendered_label": "Number of patients with foot examination",
+    },
+    321: {
+        "attribute_name": "kpi_32_1_health_check_completion_rate",
+        "rendered_label": "Care processes completion rate",
+    },
+    322: {
+        "attribute_name": "kpi_32_2_health_check_lt_12yo",
+        "rendered_label": "Care processes in patients < 12 years old",
+    },
+    323: {
+        "attribute_name": "kpi_32_3_health_check_gte_12yo",
+        "rendered_label": "Care processes in patients ≥ 12 years old",
+    },
+    33: {
+        "attribute_name": "kpi_33_hba1c_4plus",
+        "rendered_label": "Number of patients with 4 or more HbA1c measurements",
+    },
+    34: {
+        "attribute_name": "kpi_34_psychological_assessment",
+        "rendered_label": "Number of patients offered a psychological assessment",
+    },
+    35: {
+        "attribute_name": "kpi_35_smoking_status_screened",
+        "rendered_label": "Number of patients asked about smoking status",
+    },
+    36: {
+        "attribute_name": "kpi_36_referral_to_smoking_cessation_service",
+        "rendered_label": "Number of patients referred to a smoking cessation service",
+    },
+    37: {
+        "attribute_name": "kpi_37_additional_dietetic_appointment_offered",
+        "rendered_label": "Number of patients offered an additional dietetic appointment",
+    },
+    38: {
+        "attribute_name": "kpi_38_patients_attending_additional_dietetic_appointment",
+        "rendered_label": "Number of patients attending an additional dietetic appointment",
+    },
+    39: {
+        "attribute_name": "kpi_39_influenza_immunisation_recommended",
+        "rendered_label": "Number of patients recommended influenza immunisation",
+    },
+    40: {
+        "attribute_name": "kpi_40_sick_day_rules_advice",
+        "rendered_label": "Number of patients given sick day rules advice",
+    },
+    41: {
+        "attribute_name": "kpi_41_coeliac_disease_screening",
+        "rendered_label": "Number of patients with coeliac disease screening",
+    },
+    42: {
+        "attribute_name": "kpi_42_thyroid_disease_screening",
+        "rendered_label": "Number of patients with thyroid disease screening",
+    },
+    43: {
+        "attribute_name": "kpi_43_carbohydrate_counting_education",
+        "rendered_label": "Number of patients with carbohydrate counting education",
+    },
+    44: {
+        "attribute_name": "kpi_44_mean_hba1c",
+        "rendered_label": "Mean HbA1c",
+    },
+    45: {
+        "attribute_name": "kpi_45_median_hba1c",
+        "rendered_label": "Median HbA1c",
+    },
+    46: {
+        "attribute_name": "kpi_46_number_of_admissions",
+        "rendered_label": "Number of admissions",
+    },
+    47: {
+        "attribute_name": "kpi_47_number_of_dka_admissions",
+        "rendered_label": "Number of DKA admissions",
+    },
+    48: {
+        "attribute_name": "kpi_48_required_additional_psychological_support",
+        "rendered_label": "Number of patients requiring additional psychological support",
+    },
+    49: {
+        "attribute_name": "kpi_49_albuminuria_present",
+        "rendered_label": "Number of patients with albuminuria",
+    },
+}
+
+
+@dataclass
+class KPINames:
+    attribute_name: str  # e.g. kpi_32_1_health_check_completion_rate
+    rendered_label: str  # e.g. Care Processes Completion Rate
+
+
+class KPIRegistry:
+    """Makes it easier to get attribute names and rendered labels for KPIs,
+    hardcoded above.
+
+    Usage:
+
+    kpi_registry.get_kpi(323)
+        -> KPINames(
+            attribute_name='kpi_32_3_health_check_gte_12yo',
+            rendered_label='Care processes in patients ≥ 12 years old'
+        )
+
+    kpi_registry.get_attribute_name(323)
+        -> 'kpi_32_3_health_check_gte_12yo'
+
+    kpi_registry.get_rendered_label(323)
+        -> 'Care processes in patients ≥ 12 years old'
+    """
+
+    def __init__(self, kpi_data: Dict[int, Dict[str, str]]):
+        self.kpi_map = {
+            number: KPINames(data["attribute_name"], data["rendered_label"])
+            for number, data in kpi_data.items()
+        }
+
+    def get_kpi(self, number: int) -> KPINames:
+        return self.kpi_map.get(number)
+
+    def get_attribute_name(self, number: int) -> str:
+        kpi = self.get_kpi(number)
+        return kpi.attribute_name if kpi else None
+
+    def get_rendered_label(self, number: int) -> str:
+        kpi = self.get_kpi(number)
+        return kpi.rendered_label if kpi else None
+
+
+# Initialise registry
+kpi_registry = KPIRegistry(KPI_ATTRIBUTE_LABEL_MAP)
