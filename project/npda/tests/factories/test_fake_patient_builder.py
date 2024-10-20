89a06e8a4ebfd874a67a36473be280b7aec3352a
anchit-chandran <anchit97123@gmail.com>
2024-10-20 14:05:01 +0100

    add test for debugging
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/tests/factories/test_fake_patient_builder.py
+++ project/npda/tests/factories/test_fake_patient_builder.py
@@ -8,10 +8,11 @@ def test_patient_factory_creation():
     """Test Patient Factory creation."""
 
     for _ in range(10):
-        pt = PatientFactory.build(age_range=AgeRange.AGE_11_15)
+        pt = PatientFactory(age_range=AgeRange.AGE_11_15)
 
         print(f"{pt.sex=}")
         print(f"{pt.age()=}")
-        # print(f"{pt.audit_start_date=}")
+        print(f"{pt.audit_start_date=}")
         print(f"{pt.diagnosis_date=}")
+        print(f"{pt.visit.visit_date=}")
         print("")
