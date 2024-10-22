fb225a0de8ba8ced07d67b1e90d8298914c12364
reecehill <29638128+reecehill@users.noreply.github.com>
2024-10-22 03:05:43 +0100

    icons, homepage styling, responsive navbar, light and dark mode

--- project/settings.py
+++ project/settings.py
@@ -101,6 +101,7 @@ INSTALLED_APPS = [
     "two_factor.plugins.phonenumber",  # we don't use phones currently but required for app to work
     "captcha",
     "citext",
+    "fontawesomefree",
     # application
     "project.npda",
 ]
