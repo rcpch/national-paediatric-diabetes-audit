6661ed1554bb66c16d371c3646885c60d0cffb47
eatyourpeas <simon@eatyourpeas.co.uk>
2025-02-15 12:07:07 +0000

    remove async for csv_upload

---
+++ project/npda/general_functions/csv/csv_upload_celery.py
@@ -0,0 +1,259 @@
+from datetime import date
+from asgiref.sync import sync_to_async
+import logging
+import asyncio
+import collections
+import json
+
+# django imports
+from django.apps import apps
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+# third part imports
+import pandas as pd
+import numpy as np
+import httpx
+
+# RCPCH imports
+from project.constants import (
+    CSV_HEADING_OBJECTS,
+    UNIQUE_IDENTIFIER_ENGLAND,
+    UNIQUE_IDENTIFIER_JERSEY,
+)
+
+# Logging setup
+logger = logging.getLogger(__name__)
+
+from project.npda.forms.patient_form import PatientForm
+from project.npda.forms.visit_form import VisitForm
+
+
+def csv_upload(user, dataframe, csv_file_name, csv_file_bytes, pz_code, audit_year):
+    """
+    Function to upload a CSV file to the database
+    """
+    # Get the models
+    Patient = apps.get_model("npda", "Patient")
+    Transfer = apps.get_model("npda", "Transfer")
+    Visit = apps.get_model("npda", "Visit")
+    Submission = apps.get_model("npda", "Submission")
+    PaediatricDiabetesUnit = apps.get_model("npda", "PaediatricDiabetesUnit")
+
+    # Helper functions
+    def csv_value_to_model_value(model_field, value):
+        if pd.isnull(value):
+            return None
+
+        if isinstance(value, pd.Timestamp):
+            return value.to_pydatetime().date()
+
+        # Pass Django forms native Python values not numpy ones
+        # https://github.com/rcpch/national-paediatric-diabetes-audit/issues/425
+        return value.item() if isinstance(value, np.generic) else value
+
+    def row_to_dict(row, model):
+        ret = {}
+
+        for entry in CSV_HEADINGS:
+            if "model" in entry and apps.get_model("npda", entry["model"]) == model:
+                model_field_name = entry["model_field"]
+                model_field_definition = model._meta.get_field(model_field_name)
+
+                csv_value = row[entry["heading"]]
+                model_field_value = csv_value_to_model_value(
+                    model_field_definition, csv_value
+                )
+
+                ret[model_field_name] = model_field_value
+
+        return ret
+
+    def validate_transfer(row):
+        return row_to_dict(row, Transfer)
+
+    def retain_errors_and_invalid_field_data(form):
+        # We want to retain fields even if they're invalid so that we can return them to the user
+        # Use the field value from cleaned_data, falling back to data if it's not there
+        for key, value in form.cleaned_data.items():
+            setattr(form.instance, key, value)
+
+        for key, value in form.data.items():
+            if key not in form.cleaned_data:
+                setattr(form.instance, key, value)
+
+        form.instance.is_valid = form.is_valid()
+        form.instance.errors = (
+            None if form.is_valid() else form.errors.get_json_data(escape_html=True)
+        )
+
+    def record_errors_from_form(errors_to_return, row_index, form):
+        for field, errors in form.errors.as_data().items():
+            for error in errors:
+                errors_to_return[row_index][field].extend(error.messages)
+
+    if pz_code == "PZ248":
+        CSV_HEADINGS = UNIQUE_IDENTIFIER_JERSEY + CSV_HEADING_OBJECTS
+    else:
+        CSV_HEADINGS = UNIQUE_IDENTIFIER_ENGLAND + CSV_HEADING_OBJECTS
+
+    """
+    Work starts here - create a new submission and delete the previous one
+    """
+
+    # get the PDU object
+    # TODO #249 MRB: handle case where PDU does not exist
+    pdu = PaediatricDiabetesUnit.objects.get(pz_code=pz_code)
+
+    # Set previous submission to inactive
+    if Submission.objects.filter(
+        paediatric_diabetes_unit__pz_code=pdu.pz_code,
+        audit_year=audit_year,
+        submission_active=True,
+    ).exists():
+        original_submission = Submission.objects.filter(
+            submission_active=True,
+            paediatric_diabetes_unit__pz_code=pdu.pz_code,
+            audit_year=audit_year,
+        ).get()  # there can be only one of these - store it in a variable in case we need to revert
+    else:
+        original_submission = None
+
+    # Create new submission for the audit year
+    # It is not possble to create submissions in years other than the current year
+    try:
+        new_submission = Submission.objects.create(
+            paediatric_diabetes_unit=pdu,
+            audit_year=audit_year,
+            submission_date=timezone.now(),
+            submission_by=user,  # user is the user who is logged in. Passed in as a parameter
+            submission_active=True,
+            csv_file=csv_file_bytes,
+            csv_file_name=csv_file_name,
+        )
+
+        new_submission.save()
+
+    except Exception as e:
+        logger.error(f"Error creating new submission: {e}")
+        # the new submission was not created  - no action required as the previous submission is still active
+        raise ValidationError(
+            {
+                "csv_upload": "Error creating new submission. The old submission has been restored."
+            }
+        )
+
+    # now can delete all patients and visits from the previous active submission
+    if original_submission:
+        try:
+            original_submission_patient_count = Patient.objects.filter(
+                submissions=original_submission
+            ).count()
+            logger.debug(
+                f"Deleting patients from previous submission: {original_submission_patient_count}"
+            )
+            Patient.objects.filter(submissions=original_submission).delete()
+        except Exception as e:
+            raise ValidationError(
+                {"csv_upload": "Error deleting patients from previous submission"}
+            )
+
+    # now can delete the any previous active submission's csv file (if it exists)
+    # and remove the path from the field by setting it to None
+    # the rest of the submission will be retained
+    if original_submission:
+        original_submission.submission_active = False
+        try:
+            original_submission.save()  # this action will delete the csv file also as per the save method in the model
+        except Exception as e:
+            raise ValidationError(
+                {"csv_upload": "Error deactivating previous submission"}
+            )
+
+    """
+    Process the csv file and validate and save the data in the tables, parsing any errors
+    """
+
+    # Remember the original row number to help users find where the problem was in the CSV
+    dataframe = dataframe.assign(row_index=np.arange(dataframe.shape[0]))
+
+    # We only one to create one patient per NHS number (or URN if in Jersey) and we can't create their visits if we fail to save the patient model
+    if new_submission.paediatric_diabetes_unit.pz_code == "PZ248":
+        visits_by_patient = dataframe.groupby(
+            "Unique Reference Number", sort=False, dropna=False
+        )
+    else:
+        visits_by_patient = dataframe.groupby("NHS Number", sort=False, dropna=False)
+
+    # Gather all error messages indexed by row number and the field that caused them (__all__ if we don't know which one)
+    # dict[number, dict[str, list[str]]]
+    errors_to_return = collections.defaultdict(lambda: collections.defaultdict(list))
+
+    # Process each patient and their visits
+    for patient_index, patient_group in visits_by_patient:
+        patient_row = patient_group.iloc[0]
+
+        patient_dict = row_to_dict(patient_row, Patient)
+        # patient_dict["submissions"] = [new_submission]
+
+        patient_form = PatientForm(data=patient_dict)
+
+        if not patient_form.is_valid():
+            retain_errors_and_invalid_field_data(patient_form)
+            record_errors_from_form(
+                errors_to_return, patient_row["row_index"], patient_form
+            )
+
+        patient = patient_form.save()
+
+        # Save or update the patient transfer record
+        Transfer.objects.update_or_create(
+            patient=patient, paediatric_diabetes_unit=pdu, date_leaving_service=None
+        )
+
+        # Add the patient to the submission
+        new_submission.patients.add(patient)
+
+        # Process each visit for the patient
+        for visit_index, visit_row in patient_group.iterrows():
+            visit_dict = row_to_dict(visit_row, Visit)
+            visit_dict["patient"] = patient
+
+            visit_form = VisitForm(data=visit_dict, initial={"patient": patient})
+
+            if not visit_form.is_valid():
+                retain_errors_and_invalid_field_data(visit_form)
+                record_errors_from_form(
+                    errors_to_return, visit_row["row_index"], visit_form
+                )
+
+            visit = visit_form.save()
+            visit.is_valid = visit_form.is_valid()
+            visit.save()
+
+    # Store the errors to report back to the user in the Data Quality Report
+    if errors_to_return:
+        errors_to_return = convert_numpy_types(errors_to_return)
+        new_submission.errors = json.dumps(errors_to_return)
+        new_submission.save()
+
+    return errors_to_return
+
+
+def convert_numpy_types(data):
+    """
+    Recursively convert numpy types to native Python types.
+    """
+    if isinstance(data, dict):
+        return {
+            convert_numpy_types(key): convert_numpy_types(value)
+            for key, value in data.items()
+        }
+    elif isinstance(data, list):
+        return [convert_numpy_types(element) for element in data]
+    elif isinstance(data, np.generic):
+        return data.item()
+    elif isinstance(data, np.ndarray):
+        return data.tolist()
+    else:
+        return data
