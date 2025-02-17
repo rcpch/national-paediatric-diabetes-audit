c201e7d71e489dd4ecc49d30ec0a9b7704b59bed
Michael Barton <michael.barton@rcpch.ac.uk>
2025-02-17 13:52:02 +0000

    Edit permission tests

--- project/npda/tests/permissions_tests/test_patient_model_actions.py
+++ project/npda/tests/permissions_tests/test_patient_model_actions.py
@@ -144,3 +144,57 @@ def test_rcpch_audit_team_can_see_all_patients(
     pks = [patient.pk for patient in patients]
     assert(gosh_patient.pk in pks)
     assert(ah_patient.pk in pks)
+
+
+@pytest.mark.django_db
+def test_users_can_only_edit_patients_from_their_own_pdu(
+    seed_groups_fixture,
+    seed_users_fixture,
+    client,
+):
+    gosh_user = NPDAUser.objects.filter(
+        organisation_employers__pz_code=GOSH_PZ_CODE
+    ).first()
+
+    ah_user = NPDAUser.objects.filter(
+        organisation_employers__pz_code=ALDER_HEY_PZ_CODE
+    ).first()
+
+    ah_patient = create_submission_with_patient(ah_user)
+
+    client = login_and_verify_user(client, gosh_user)
+
+    url = reverse("patient-update", args=[ah_patient.pk])
+    response = client.get(url)
+
+    assert(response.status_code == HTTPStatus.FORBIDDEN)
+
+
+@pytest.mark.django_db
+def test_rcpch_audit_team_can_edit_patients_from_any_pdu(
+    seed_groups_fixture,
+    seed_users_fixture,
+    client,
+):
+    gosh_user = NPDAUser.objects.filter(
+        organisation_employers__pz_code=GOSH_PZ_CODE
+    ).first()
+
+    ah_user = NPDAUser.objects.filter(
+        organisation_employers__pz_code=ALDER_HEY_PZ_CODE
+    ).first()
+
+    rcpch_user = NPDAUser.objects.filter(
+        is_rcpch_audit_team_member=True
+    ).first()
+
+    gosh_patient = create_submission_with_patient(gosh_user)
+    ah_patient = create_submission_with_patient(ah_user)
+
+    client = login_and_verify_user(client, rcpch_user)
+
+    gosh_url = reverse("patient-update", args=[gosh_patient.pk])
+    assert(client.get(gosh_url).status_code == HTTPStatus.OK)
+
+    ah_url = reverse("patient-update", args=[ah_patient.pk])
+    assert(client.get(ah_url).status_code == HTTPStatus.OK)
