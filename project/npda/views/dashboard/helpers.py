40b50a9b14bdfce26427553cc0921d73445f094a
anchit-chandran <anchit97123@gmail.com>
2025-02-26 17:31:30 +0000

    update labels
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- project/npda/views/dashboard/helpers.py
+++ project/npda/views/dashboard/helpers.py
@@ -104,11 +104,11 @@ def get_hc_completion_rate_vcs(
     vcs = {}
     for ix, kpi_values in enumerate([kpi_32_1_values, kpi_32_2_values, kpi_32_3_values], start=1):
         if ix == 1:
-            kpi_label = "<12 years old"
-        elif ix == 2:
-            kpi_label = ">=12 years old"
-        else:
             kpi_label = "Overall"
+        elif ix == 2:
+            kpi_label = "<12 years old"
+        else:
+            kpi_label = ">=12 years old"
 
         vcs[f"kpi_32_{ix}_values"] = {
             "count": kpi_values["total_passed"],
@@ -120,7 +120,7 @@ def get_hc_completion_rate_vcs(
             ),
             "label": kpi_label,
         }
-
+    logger.debug(f'{vcs=}')
     return vcs
 
 
