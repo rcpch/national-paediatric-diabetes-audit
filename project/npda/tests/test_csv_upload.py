dcf874abbb8dedd257527a1eef29b767c56762ca
Michael Barton <michael.barton@rcpch.ac.uk>
2025-03-17 15:02:28 +0000

    Do all the other old npda headings

--- project/npda/tests/test_csv_upload.py
+++ project/npda/tests/test_csv_upload.py
@@ -924,8 +924,8 @@ def test_invalid_date_of_birth_column_name_with_mixed_case_column_headers(
     )
     results = read_csv_from_str(csv)
 
-    assert results.missing_columns == ["Date of Birth"]
-    assert results.additional_columns == ["DOB"]
+    assert results.missing_columns == []
+    assert results.additional_columns == []
 
 
 # https://github.com/rcpch/national-paediatric-diabetes-audit/issues/741
