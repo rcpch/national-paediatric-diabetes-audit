c2aaaa9ac0187e55cb7ed101551bb6825dd08b8d
Michael Barton <michael.barton@rcpch.ac.uk>
2025-03-18 11:27:40 +0000

    Fix up clean_hospital_admission_date

--- project/npda/forms/visit_form.py
+++ project/npda/forms/visit_form.py
@@ -700,13 +700,7 @@ class VisitForm(forms.ModelForm):
 
     def clean_hospital_admission_date(self):
         data = self.cleaned_data["hospital_admission_date"]
-        diagnosis_date = date(
-            year=self.patient.diagnosis_date.year,
-            month=self.patient.diagnosis_date.month,
-            day=self.patient.diagnosis_date.day,
-        )
-        # diagnosis date can be within 11 days of admission date
-        diagnosis_date = diagnosis_date - timedelta(days=11)
+
         valid, error = validate_date(
             date_under_examination_field_name="hospital_admission_date",
             date_under_examination_label_name="Start date (Hospital Provider Spell)",
