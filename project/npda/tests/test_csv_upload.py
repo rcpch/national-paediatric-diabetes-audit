646af900ced33681d84f7b5524eca125b1f395f6
Michael Barton <michael.barton@rcpch.ac.uk>
2025-03-18 15:58:54 +0000

    Failing tests for stated gender options

--- project/npda/tests/test_csv_upload.py
+++ project/npda/tests/test_csv_upload.py
@@ -568,6 +568,33 @@ def test_invalid_sex(test_user, single_row_valid_df):
 
 
 @pytest.mark.django_db
+def test_not_specified_sex(test_user, single_row_valid_df):
+    single_row_valid_df["Stated gender"] = 3
+
+    errors = csv_upload_sync(test_user, single_row_valid_df)
+    assert "sex" in errors[0]
+
+    patient = Patient.objects.first()
+
+    assert patient.sex == 3
+    assert len(patient.errors) == 0
+
+
+@pytest.mark.django_db
+def test_unknown_sex(test_user, single_row_valid_df):
+    single_row_valid_df["Stated gender"] = 99
+
+    errors = csv_upload_sync(test_user, single_row_valid_df)
+    assert "sex" in errors[0]
+
+    patient = Patient.objects.first()
+
+    assert patient.sex == 99
+    assert len(patient.errors) == 0
+
+
+
+@pytest.mark.django_db
 def test_missing_gp_ods_code(test_user, single_row_valid_df):
     single_row_valid_df["GP Practice Code"] = None
 
