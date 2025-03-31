6c84f97f8cf5a93d4eee0868ad27f9a854a247fa
Danny Cowen <danny.cowen9821@gmail.com>
2025-03-31 16:53:18 +0100

    Define constants list of headers for breadcrumbs

---
+++ project/constants/navbar_headers.py
@@ -0,0 +1,9 @@
+NAVBAR_HEADERS = {
+    "dashboard": "Unit Report",
+    "patient_report": "Patient Report",
+    "patients": "Patient Data",
+    "upload_csv": "Upload CSV",
+    "submissions": "Submissions",
+    "patient": "Patient Info",
+    "visits": "Visits"
+}
