ae9228e51339cd3145aab3cf1f6be1e9ec85cb1a
Michael Barton <michael.barton@rcpch.ac.uk>
2025-04-01 13:45:38 +0100

    Totally un-hardcode audit year from test

--- project/npda/tests/permissions_tests/test_patient_model_actions.py
+++ project/npda/tests/permissions_tests/test_patient_model_actions.py
@@ -9,6 +9,7 @@ from http import HTTPStatus
 # RCPCH imports
 from project.constants.user import RCPCH_AUDIT_TEAM
 from project.npda.models import Patient, Submission, NPDAUser
+from project.npda.general_functions import get_current_audit_year
 from project.npda.tests.utils import login_and_verify_user
 from project.npda.tests.factories.patient_factory import PatientFactory
 from project.npda.tests.factories.visit_factory import VisitFactory
@@ -19,13 +20,9 @@ ALDER_HEY_PZ_CODE = "PZ074"
 
 
 def create_submission_with_patient(user):
-    # HACK: Workaround this test depending on the default audit year
-    #       that is set after logging in.
-    #       https://github.com/rcpch/national-paediatric-diabetes-audit/issues/876
-    #       See you next year unless addressed in https://github.com/rcpch/national-paediatric-diabetes-audit/pull/865.
     submission = Submission.objects.create(
-        audit_year=2025,
-        submission_date="2025-04-01T00:00:00Z",
+        audit_year=get_current_audit_year(),
+        submission_date=f"{get_current_audit_year()}-04-01T00:00:00Z",
         submission_active=True,
         submission_by=user,
         paediatric_diabetes_unit=user.organisation_employers.first(),
