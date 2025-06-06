e555bb9de1f570cb789943cf3902ba27a73c3065
eatyourpeas <simon@eatyourpeas.co.uk>
2025-06-06 23:38:29 +0100

    strip cm / kg from height and weight

--- project/npda/general_functions/csv/csv_clean.py
+++ project/npda/general_functions/csv/csv_clean.py
@@ -23,13 +23,41 @@ def clean_csv_sex(value):
             # Return Not Known. The patient won't save in the database without a numeric value.
             return 0
 
+def clean_csv_measurement(value):
+    """
+    Convert measurement values to float, handling empty strings and non-numeric values.
+    Remove extraneous non-numeric characters if necessary.
+    Returns None for empty strings or non-numeric values.
+    If the value is a valid number, it will be converted to float.
+    If the value is an empty string, it will return None.
+    If the value cannot be converted to float, it will return None.
+    """
+    if value == "":
+        return None
+    if isinstance(value, str):
+        # Remove any non-numeric characters except for decimal points
+        value = ''.join(char for char in value if char.isdigit() or char == '.')
+    
+    try:
+        return float(value)
+    except ValueError:
+        return None
+
 
 def csv_clean(df):
     if not is_numeric_dtype(df["Stated gender"]):
         df["Stated gender"] = df["Stated gender"].apply(clean_csv_sex)
     
+    # strip whitespace and convert height and weight to numeric, removing any non-numeric characters (eg, "cm", "kg")
+    if not is_numeric_dtype(df["Patient Height (cm)"]):
+        df["Patient Height (cm)"] = df["Patient Height (cm)"].apply(clean_csv_measurement)
+    if not is_numeric_dtype(df["Patient Weight (kg)"]):
+        df["Patient Weight (kg)"] = df["Patient Weight (kg)"].apply(clean_csv_measurement)
+    
     # Strip whitespace only fields of whitespaces and replace with None
     for col in df.select_dtypes(include=['object']).columns:
         df[col] = df[col].astype(str).str.strip().replace('', None)
+    
+
 
     return df
