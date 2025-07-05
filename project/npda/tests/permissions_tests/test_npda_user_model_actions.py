8217bd64401b68b126e4168031ec6c4c5565dac0
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-05 13:49:17 +0100

    Failing tests for is_superuser

--- project/npda/tests/permissions_tests/test_npda_user_model_actions.py
+++ project/npda/tests/permissions_tests/test_npda_user_model_actions.py
@@ -582,6 +582,70 @@ def test_audit_team_can_add_employers_outside_of_their_pdu(
     assert employers == { ALDER_HEY_PZ_CODE, GOSH_PZ_CODE }
 
 
+# https://github.com/rcpch/national-paediatric-diabetes-audit/issues/911
+@pytest.mark.django_db
+def test_coordinators_cannot_set_user_flags(
+    seed_groups_fixture,
+    seed_users_fixture,
+    seed_audit_periods_fixture,
+    client,
+):
+    coordinator = NPDAUser.objects.filter(
+        organisation_employers__pz_code=ALDER_HEY_PZ_CODE, role=AUDIT_CENTRE_COORDINATOR
+    ).first()
+
+    assert coordinator.is_superuser is False
+
+    client = login_and_verify_user(client, coordinator)
+
+    url = reverse("npdauser-update", kwargs={"pk": coordinator.pk})
+
+    response = client.post(url, data={
+        "add_employer": ALDER_HEY_PZ_CODE,
+        "first_name": coordinator.first_name,
+        "surname": coordinator.surname,
+        "email": coordinator.email,
+        "role": coordinator.role,
+        "is_superuser": "on",
+    })
+
+    coordinator.refresh_from_db()
+    assert coordinator.is_superuser is False
+
+
+@pytest.mark.django_db
+def test_coordinators_cannot_create_users_with_flags(
+    seed_groups_fixture,
+    seed_users_fixture,
+    seed_audit_periods_fixture,
+    client,
+):
+    user_count_before = NPDAUser.objects.count()
+
+    coordinator = NPDAUser.objects.filter(
+        organisation_employers__pz_code=ALDER_HEY_PZ_CODE, role=AUDIT_CENTRE_COORDINATOR
+    ).first()
+
+    client = login_and_verify_user(client, coordinator)
+
+    url = reverse("npdauser-create")
+
+    client.post(
+        url,
+        {
+            "first_name": "Bob",
+            "surname": "Bobertson",
+            "email": "bob@bobertson.com",
+            "add_employer": ALDER_HEY_PZ_CODE,
+            "role": AUDIT_CENTRE_COORDINATOR,
+            "is_superuser": "on"
+        },
+    )
+
+    user_count_after = NPDAUser.objects.count()
+    assert user_count_after == user_count_before
+
+
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     "user_data",
