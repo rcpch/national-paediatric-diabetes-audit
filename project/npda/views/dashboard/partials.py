c267478bff7f8df307939c5fb816337cbb1829c7
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-12 15:46:59 +0100

    Use submission helper for map

--- project/npda/views/dashboard/partials.py
+++ project/npda/views/dashboard/partials.py
@@ -42,32 +42,21 @@ def get_map_chart_partial(request):
     if not request.htmx:
         return HttpResponseBadRequest("This view is only accessible via HTMX")
 
-    # Fetch data from query parameters
-    pz_code: str = request.session.get("pz_code")
-    selected_audit_year = request.session.get("selected_audit_year")
+    submission = Submission.objects.get_submission_for_request(request)
+    lead_organisation_ods_code = submission.paediatric_diabetes_unit.lead_organisation_ods_code
 
     try:
-        paediatric_diabetes_unit = PaediatricDiabetesUnitClass.objects.get(
-            pz_code=pz_code
-        )
-
-        # get lead organisation for the selected PDU
         pdu_lead_organisation = fetch_organisation_by_ods_code(
-            ods_code=paediatric_diabetes_unit.lead_organisation_ods_code
+            ods_code=lead_organisation_ods_code
         )
     except:
         raise ValueError(
-            f"Lead organisation for PDU {paediatric_diabetes_unit.lead_organisation_ods_code=} not found"
+            f"Lead organisation for PDU {lead_organisation_ods_code=} not found"
         )
 
     try:
-
-        # thes are all registered patients for the current cohort at the selected organisation to be plotted in the map
-        patients_to_plot = get_children_by_pdu_audit_year(
-            paediatric_diabetes_unit=paediatric_diabetes_unit,
-            paediatric_diabetes_unit_lead_organisation=pdu_lead_organisation,
-            audit_year=selected_audit_year,
-        )
+        # these are all registered patients for the current cohort at the selected organisation to be plotted in the map
+        patients_to_plot = get_children_by_pdu_audit_year(submission,pdu_lead_organisation)
 
         # aggregated distances (mean, median, max, min) that patients have travelled to the selected organisation
         aggregated_distances, patient_distances_dataframe = (
@@ -81,7 +70,7 @@ def get_map_chart_partial(request):
             generate_distance_from_organisation_scatterplot_figure(
                 geo_df=patient_distances_dataframe,
                 pdu_lead_organisation=pdu_lead_organisation,
-                paediatric_diabetes_unit=paediatric_diabetes_unit,
+                paediatric_diabetes_unit=submission.paediatric_diabetes_unit,
             )
         )
 
