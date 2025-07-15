e293d474a56fcb946ef447c6df83aa4deab354cc
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-15 15:02:21 +0100

    Try as a separate migration

--- project/npda/migrations/0038_auditperiod_slug.py
+++ project/npda/migrations/0038_auditperiod_slug.py
@@ -21,10 +21,5 @@ class Migration(migrations.Migration):
             field=models.SlugField(default="-"),
             preserve_default=False,
         ),
-        migrations.RunPython(get_audit_period_slug, migrations.RunPython.noop),
-        migrations.AlterField(
-            model_name="auditperiod",
-            name="slug",
-            field=models.SlugField(unique=True),
-        ) 
+        migrations.RunPython(get_audit_period_slug, migrations.RunPython.noop), 
     ]
