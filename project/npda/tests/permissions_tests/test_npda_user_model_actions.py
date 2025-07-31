cf2370d97b4ff1d7e196aa974c2a484441f11035
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-31 18:38:41 +0100

    Fix tests

--- project/npda/tests/permissions_tests/test_npda_user_model_actions.py
+++ project/npda/tests/permissions_tests/test_npda_user_model_actions.py
@@ -193,40 +193,6 @@ def test_npda_user_list_view_normal_users_cannot_set_their_view_preference_to_na
         headers={"HX-Request": "true"},
     )
 
-    assert set_view_preference_response.status_code == HTTPStatus.FORBIDDEN
-
-    # Check the session isn't modified anyway
-    response = client.get(reverse("npda_users"))
-    assert response.status_code == HTTPStatus.OK
-
-    users = response.context_data["object_list"]
-    check_all_users_in_pdu(ah_user, users, ALDER_HEY_PZ_CODE)
-
-
-@pytest.mark.django_db
-def test_npda_user_list_view_users_cannot_set_their_view_preference_to_organisation(
-    seed_groups_fixture,
-    seed_users_fixture,
-    seed_audit_periods_fixture,
-    client,
-):
-    ah_user = NPDAUser.objects.filter(
-        organisation_employers__pz_code=ALDER_HEY_PZ_CODE
-    ).first()
-
-    client = login_and_verify_user(client, ah_user)
-
-    set_view_preference_response = client.post(
-        reverse("view_preference"),
-        {"view_preference": 0},
-        headers={"HX-Request": "true"},
-    )
-
-    assert set_view_preference_response.status_code == HTTPStatus.BAD_REQUEST
-
-    ah_user.refresh_from_db()
-    assert ah_user.view_preference == 1
-
     # Check the session isn't modified anyway
     response = client.get(reverse("npda_users"))
     assert response.status_code == HTTPStatus.OK
