ded335a084930066e41c058838183d04f5f10635
Michael Barton <michael.barton@rcpch.ac.uk>
2025-09-10 09:58:20 +0100

    Add feature flag to hide users

--- project/constants/feature_flags.py
+++ project/constants/feature_flags.py
@@ -2,5 +2,9 @@ FEATURE_FLAGS = {
     "new_navigation": {
         "description": "Replace old navigation with switching PDU on the homepage and breadcrumbs in the header",
         "default": True
+    },
+    "hide_users_other_than_test": {
+        "description": "Hide all users except test users from the user list - useful for demos to avoid showing personal data",
+        "default": False
     }
 }
