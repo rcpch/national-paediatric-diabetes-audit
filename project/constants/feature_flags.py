a8e4a17af26938646280bf007485f16968dbb850
Michael Barton <michael@mbarton.cc>
2025-10-14 15:36:57 +0100

    Remove new navigation feature flag

--- project/constants/feature_flags.py
+++ project/constants/feature_flags.py
@@ -1,8 +1,4 @@
 FEATURE_FLAGS = {
-    "new_navigation": {
-        "description": "Replace old navigation with switching PDU on the homepage and breadcrumbs in the header",
-        "default": True
-    },
     "hide_users_other_than_test": {
         "description": "Hide all users except test users from the user list - useful for demos to avoid showing personal data",
         "default": False
