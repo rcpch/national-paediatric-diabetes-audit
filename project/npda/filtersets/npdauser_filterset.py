156f3665cbd2b76b19df2ab0118b78f5f5f94b64
eatyourpeas <simon@eatyourpeas.co.uk>
2025-11-08 13:01:05 +0000

    updates the npda_user filterset to filter by network and pdu

--- project/npda/filtersets/npdauser_filterset.py
+++ project/npda/filtersets/npdauser_filterset.py
@@ -4,27 +4,37 @@ from django.db.models import Q
 from project.npda.models import NPDAUser
 from django_filters import FilterSet, CharFilter
 
-class NPDAUserFilterSet(FilterSet):
 
+class NPDAUserFilterSet(FilterSet):
     search = CharFilter(
-        method='filter_search',
-        widget=TextInput(attrs={
-            'placeholder': 'Search by email, first name, or surname',
-            'class': 'form-control text-center placeholder-gray-300 focus:bg-rcpch_strong_blue_light_tint2 hover:bg-rcpch_strong_blue_light_tint2 focus:placeholder-gray-500 p-2 m-2 w-full',
-        }),
-        label='Search users',
+        method="filter_search",
+        widget=TextInput(
+            attrs={
+                "placeholder": "Search by email, first name, surname, PDU (PZ code or lead organisation name) or Network (PN Code or name)...",
+                "class": "form-control text-center placeholder-gray-300 focus:bg-rcpch_strong_blue_light_tint2 hover:bg-rcpch_strong_blue_light_tint2 focus:placeholder-gray-500 p-2 m-2 w-full",
+            }
+        ),
+        label="Search users",
     )
 
     class Meta:
         model = NPDAUser
-        fields=[]
-    
+        fields = []
+
     def filter_search(self, queryset, name, value):
         if value:
             queryset = queryset.filter(
-                Q(pk__icontains=value) |
-                Q(email__icontains=value) |
-                Q(first_name__icontains=value) |
-                Q(surname__icontains=value)
+                Q(pk__icontains=value)
+                | Q(email__icontains=value)
+                | Q(first_name__icontains=value)
+                | Q(surname__icontains=value)
+                | Q(organisation_employers__pz_code__icontains=value)
+                | Q(organisation_employers__lead_organisation_name__icontains=value)
+                | Q(
+                    organisation_employers__paediatric_diabetes_network_code__icontains=value
+                )
+                | Q(
+                    organisation_employers__paediatric_diabetes_network_name__icontains=value
+                )
             )
         return queryset
