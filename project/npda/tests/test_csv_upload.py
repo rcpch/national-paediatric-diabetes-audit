5379c67eb958010fd6d749d7ff87733ca99332bf
Michael Barton <michael.barton@rcpch.ac.uk>
2026-01-23 10:41:57 +0000

    Failing test for incorrect pdu

--- project/npda/tests/test_csv_upload.py
+++ project/npda/tests/test_csv_upload.py
@@ -93,6 +93,7 @@ def mock_remote_calls():
 
 
 ALDER_HEY_PZ_CODE = "PZ074"
+RCPCH_PZ_CODE = "PZ999"
 
 
 @pytest.fixture
@@ -4188,3 +4189,37 @@ def test_thyroid_and_coeliac_dates_earlier_than_90_days_before_diagnosis_fail_va
 
     assert "coeliac_screen_date" in errors[0]
     assert "thyroid_function_date" in errors[0]
+
+
+@pytest.mark.django_db
+def test_uploading_csv_against_incorrect_pdu(
+    single_row_valid_df,
+    tmp_path,
+    client,
+    test_rcpch_user
+):
+    single_row_valid_df["PDU Number"] = RCPCH_PZ_CODE
+
+    # write back into temp
+    tmp_csv_path = tmp_path / "dummy_sheet_test_csv_upload_test_uploading_csv_against_incorrect_pdu.csv"
+    single_row_valid_df.to_csv(tmp_csv_path, index=False)
+
+    # Log in user
+    client = login_and_verify_user(client, test_rcpch_user)
+
+    url = reverse("pdu-upload-csv", kwargs={ "pz_code": ALDER_HEY_PZ_CODE, "audit_period": "2025-2026"})
+
+    # Feed file to view
+    with open(tmp_csv_path, "rb") as csv_file:
+        response = client.post(
+            url,
+            {
+                'csv_upload': csv_file
+            },
+            format='multipart'
+        )
+
+    assert response.status_code == 200
+    assert "Warning: You done did a fall over" in response.content.decode("utf-8")
+
+    assert Submission.objects.count() == 0, "No submission should be created if the PDU number is incorrect"
