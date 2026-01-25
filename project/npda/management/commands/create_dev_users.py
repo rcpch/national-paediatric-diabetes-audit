84e9eeb403630878976059c9b7c74ddc7bc6909f
Michael Barton <michael.barton@rcpch.ac.uk>
2026-01-25 20:47:08 +0000

    Create split unit dev user

--- project/npda/management/commands/create_dev_users.py
+++ project/npda/management/commands/create_dev_users.py
@@ -11,21 +11,23 @@ from project.constants.user import (
     RCPCH_AUDIT_TEAM,
 )
 
+from project.npda.models import PaediatricDiabetesUnit, OrganisationEmployer
+
 class Command(BaseCommand):
     help = "Create test users using the base set in environment variables: LOCAL_DEV_ADMIN_EMAIL and LOCAL_DEV_ADMIN_PASSWORD."
 
-    def create_user_if_not_exists(self, email, first_name, surname, password, role, is_rcpch_audit_team_member=False):
+    def create_user_if_not_exists(self, pz_code, email, first_name, surname, password, role, is_rcpch_audit_team_member=False):
         user_model = get_user_model()
 
         if not user_model.objects.filter(email=email).exists():
-            user_model.objects.create_or_update_user(
+            user = user_model.objects.create_or_update_user(
                 first_name=first_name,
                 surname=surname,
                 email=email,
                 password=password,
                 role=role,
                 is_active=True,
-                pz_code="PZ999",  # RCPCH
+                pz_code=pz_code,
                 is_primary_employer=True,
                 is_rcpch_audit_team_member=is_rcpch_audit_team_member
             )
@@ -33,6 +35,9 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f"Successfully created {email}."))
         else:
             self.stdout.write(self.style.WARNING(f"{email} already exists."))
+            user = user_model.objects.get(email=email)
+        
+        return user
 
     def handle(self, *args, **kwargs):
         user_model = get_user_model()
@@ -64,6 +69,7 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING(f"{local_dev_admin_email} already exists."))
         
         self.create_user_if_not_exists(
+            pz_code="PZ999",
             email=coordinator_dev_email,
             first_name="CoordinatorAda",
             surname="Lovelace",
@@ -72,6 +78,7 @@ class Command(BaseCommand):
         )
 
         self.create_user_if_not_exists(
+            pz_code="PZ999",
             email=editor_dev_email,
             first_name="EditorAda",
             surname="Lovelace",
@@ -80,6 +87,7 @@ class Command(BaseCommand):
         )
 
         self.create_user_if_not_exists(
+            pz_code="PZ999",
             email=reader_dev_email,
             first_name="ReaderAda",
             surname="Lovelace",
@@ -88,6 +96,7 @@ class Command(BaseCommand):
         )
 
         self.create_user_if_not_exists(
+            pz_code="PZ999",
             email=rcpch_audit_team_email,
             first_name="RCPCHAuditTeamAda",
             surname="Lovelace",
@@ -95,5 +104,23 @@ class Command(BaseCommand):
             role=RCPCH_AUDIT_TEAM,
             is_rcpch_audit_team_member=True
         )
-        
+
+        # Test inactive split unit (https://github.com/rcpch/national-paediatric-diabetes-audit/issues/924)
+        # PZ003 was split into PZ251 and PZ252
+        split_user = self.create_user_if_not_exists(
+            pz_code="PZ251",
+            email=f"{local_part}+split@{domain}",
+            first_name="SplitUnitAda",
+            surname="Lovelace",
+            password=password,
+            role=AUDIT_CENTRE_COORDINATOR,
+        )
+
+        OrganisationEmployer.objects.create(
+            paediatric_diabetes_unit=PaediatricDiabetesUnit.objects.get(pz_code="PZ003"),
+            npda_user=split_user,
+            is_primary_employer=False,
+        )
+
+
 
