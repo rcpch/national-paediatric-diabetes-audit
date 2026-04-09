6b7b1a4f553fd2d323653a82d8a2f5fb3d0dda80
Michael Barton <michael.barton@rcpch.ac.uk>
2026-04-09 21:18:08 +0100

    Tweak assertion to cover the old 2021 tests that are full of unrelated errors

--- project/npda/tests/test_csv_upload.py
+++ project/npda/tests/test_csv_upload.py
@@ -4654,7 +4654,7 @@ def test_case_insensitive_ethnic_category(test_user, dummy_sheet_csv):
     df = read_csv_from_str(one_row_csv).df
 
     errors = csv_upload_sync(test_user, df)
-    assert len(errors) == 0
+    assert "ethnicity" not in errors[0]
 
     patient = Patient.objects.first()
 
