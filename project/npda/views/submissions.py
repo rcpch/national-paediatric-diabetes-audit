7f49378a0272430c8a8ffb2ec6cb001d7292028e
Michael Barton <mbarton@users.noreply.github.com>
2026-06-26 15:35:42 +0100

    Change log level from info to error for CSV upload
    This emails me so I can investigate promptly. Follow up from #1456

--- project/npda/views/submissions.py
+++ project/npda/views/submissions.py
@@ -482,7 +482,7 @@ def upload_csv(request, audit_period, pdu):
             or parsed_csv.additional_columns
             or parsed_csv.duplicate_columns
         ):
-            logger.info(
+            logger.error(
                 "CSV upload failed due to column issues. pz_code=%s missing_columns=%s additional_columns=%s duplicate_columns=%s",
                 pz_code,
                 missing_columns,
