605edd4a6180423a168be78fa55ba8fc3cb0a5c3
Michael Barton <michael.barton@rcpch.ac.uk>
2026-07-15 12:58:56 +0100

    Fix patient report crash on invalid hba1c value

--- project/npda/general_functions/patient_report/queries.py
+++ project/npda/general_functions/patient_report/queries.py
@@ -1434,21 +1434,15 @@ def calculate_hba1c_values(qs, audit_period):
 
             patient["kpi_44_mean_hba1c"] = round(mean_val)
             patient["kpi_45_median_hba1c"] = round(median_val)
-            patient["mean_hba1c_pct"] = round(
-                (
-                    (Decimal("0.09148") * mean_val) + Decimal("2.152")
-                    if mean_val > 0
-                    else None
-                ),
-                1,
+            patient["mean_hba1c_pct"] = (
+                round((Decimal("0.09148") * mean_val) + Decimal("2.152"), 1)
+                if mean_val > 0
+                else None
             )
-            patient["median_hba1c_pct"] = round(
-                (
-                    (Decimal("0.09148") * median_val) + Decimal("2.152")
-                    if median_val > 0
-                    else None
-                ),
-                1,
+            patient["median_hba1c_pct"] = (
+                round((Decimal("0.09148") * median_val) + Decimal("2.152"), 1)
+                if median_val > 0
+                else None
             )
         else:
             patient["kpi_44_mean_hba1c"] = None
